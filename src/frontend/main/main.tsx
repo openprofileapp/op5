92ca5d79d927c3fa321b20c5d42b8801df2d8d2e
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-05-15 12:26:32 -0400

    updated imports

--- src/frontend/main/main.tsx
+++ src/frontend/main/main.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import { I18nextProvider } from 'react-i18next'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { HelmetProvider } from "react-helmet-async"
+import { I18nextProvider } from "react-i18next"
 
-import i18n from '../_common/i18n.js'
+import i18n from "../_common/i18n.js"
 
 import "../_common/styles/tailwind.css";
 import "../_common/styles/app.css"
 import "./scripts/main.js";
 
-import Home from './pages/Home.js'
-import Search from './pages/Search.js'
-import Partners from './pages/Partners.js'
-import Premium from './pages/Premium.js'
+import Home from "./pages/Home.js"
+import Search from "./pages/Search.js"
+import Browse from "./pages/Browse.js"
+import Partners from "./pages/Partners.js"
+import Premium from "./pages/Premium.js"
 
-import ComingSoon from './pages/ComingSoon.js'
-import NotFound from './pages/NotFound.js'
+import ComingSoon from "./pages/ComingSoon.js"
+import NotFound from "./pages/NotFound.js"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <HelmetProvider>
             <I18nextProvider i18n={i18n}>
@@ -30,6 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                         <Route path="/library" element={<ComingSoon />} />
                         <Route path="/partners" element={<Partners />} />
                         <Route path="/premium" element={<Premium />} />
+                        <Route path="/popular" element={<Browse />} />
                         
                         <Route path="/404" element={<NotFound />} />
                         <Route path="*" element={<Navigate to="/404" replace />} />
