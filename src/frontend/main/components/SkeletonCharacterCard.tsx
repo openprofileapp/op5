3c64dfbc27c5b66a9a7736db885b9d5cd96408e8
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-05-15 12:27:21 -0400

    added SkeletonCharacterCard.tsx

---
+++ src/frontend/main/components/SkeletonCharacterCard.tsx
@@ -0,0 +1,17 @@
+export default function SkeletonCharacterCard() {
+    return (
+        <div className="card-tall flex w-52 flex-col gap-4"
+            style={{ backgroundColor: "#00000000" }}    
+        >
+            <div className="skeleton h-55 w-full"></div>
+            <div className="skeleton rounded-full h-4 w-full"></div>
+            <div className="skeleton rounded-full h-4 w-full"></div>
+            <div className="skeleton rounded-full h-4 w-full"></div>
+            <div className="skeleton rounded-full h-4 w-full"></div>
+            <div className="flex gap-4">
+                <div className="skeleton rounded-full h-8 w-1/2"></div>
+                <div className="skeleton rounded-full h-8 w-1/2"></div>
+            </div>
+        </div>
+    );
+}
