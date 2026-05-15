18655916d630733fa1d2dff99644029ee827bb5a
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-05-15 12:28:42 -0400

    updated characters

--- src/frontend/support/main.tsx
+++ src/frontend/support/main.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import { I18nextProvider } from 'react-i18next'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { HelmetProvider } from "react-helmet-async"
+import { I18nextProvider } from "react-i18next"
 
-import i18n from '../_common/i18n.js'
+import i18n from "../_common/i18n.js"
 
 import "../_common/styles/tailwind.css";
 import "../_common/styles/daisyui.css";
 import "./scripts/main.js";
 
 import About from "./pages/About.js"
-import Home from './pages/Home.js'
-import NotFound from './pages/NotFound.js'
+import Home from "./pages/Home.js"
+import NotFound from "./pages/NotFound.js"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <HelmetProvider>
             <I18nextProvider i18n={i18n}>
