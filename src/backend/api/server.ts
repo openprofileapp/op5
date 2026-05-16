aa4a9dccc8b1c51ca85bcabf0d7e8031253b0275
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-05-16 14:24:12 -0400

    added data migration

--- src/backend/api/server.ts
+++ src/backend/api/server.ts
@@ -17,6 +17,7 @@ import terminateApp from "../../_common/helpers/terminateApp.js";
 import { corsMiddleware } from '../_common/middlewares/cors.middleware.js';
 import { maintenanceMiddleware } from '../_common/middlewares/maintenance.middleware.js';
 import userRoutes from './routes/user.routes.js';
+import profileRoutes from './routes/profile.routes.js';
 
 /* 
 ————————————————————————————————————————————————————————————————
@@ -25,11 +26,191 @@ Connect databases
 */
 
 export const db = {
-    metadata: new Database("data/databases/metadata.sqlite")
+    metadata: new Database("data/databases/metadata.sqlite"),
+    characters: new Database("data/databases/characters.sqlite"),
+    accounts: new Database("data/databases/accounts.sqlite"),
+    badges: new Database("data/databases/badges.sqlite")
 };
 
-db.metadata.transaction((query) => {
-    if (!query("SELECT * FROM metadata LIMIT 1").success) { query(`${config.folders.sql}/metadata.sql`); };
+db.metadata.transaction(q => {
+    if (!q("SELECT * FROM metadata LIMIT 1").success) { q(`${config.folders.sql}/metadata.sql`); };
+});
+
+db.characters.transaction(q => {
+    if (!q("SELECT * FROM published LIMIT 1").success) { q(`${config.folders.sql}/characters/published.sql`); };
+});
+
+db.accounts.transaction(q => {
+    if (!q("SELECT * FROM public LIMIT 1").success) { q(`${config.folders.sql}/accounts/public.sql`); };
+});
+
+db.badges.transaction(q => {
+    if (!q("SELECT * FROM badges LIMIT 1").success) { q(`${config.folders.sql}/badges.sql`); };
+});
+
+/* 
+————————————————————————————————————————————————————————————————
+Migrade databases from v5.0.237 to v5.0.300
+———————————————————————————————————————————————————————————————— 
+*/
+
+export const mdb = {
+    profiles: new Database("data/databases/v5.0.237/profiles.db"),
+    accounts: new Database("data/databases/v5.0.237/accounts.db")
+};
+
+// profiles.db/published -> characters.sqlite
+const mdbProfilesPublishedData = mdb.profiles.query("SELECT * from published");
+
+db.characters.transaction(q => {
+    if (!mdbProfilesPublishedData.success) return;
+
+    for (const d of mdbProfilesPublishedData.rows) {
+        q(
+            `INSERT INTO published (
+                id, 
+                owner, 
+                slug,
+                displayName,
+                avatar,
+                banner,
+                about,
+                tags,
+                license,
+                licenseContact,
+                isAuraEnabled,
+                auraType,
+                auraPrimary,
+                auraSecondary,
+                explicit,
+                visibility,
+                isScheduled,
+                updatedDate,
+                createdDate
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [
+                d.id,
+                d.owner,
+                d.url,
+                d.display_name,
+                d.avatar,
+                d.banner,
+                d.about,
+                d.tags,
+                d.license,
+                d.license_contact,
+                d.aura || 0,
+                "flow",
+                d.aura_primary,
+                d.aura_secondary,
+                d.explicit,
+                d.visibility || "public",
+                0,
+                d.updated_date,
+                d.created_date
+            ]
+        );
+    }
+});
+
+// accounts.db/public -> accounts.sqlite
+const mdbAccountsPublicData = mdb.accounts.query("SELECT * from public");
+
+db.accounts.transaction(q => {
+    if (!mdbAccountsPublicData.success) return;
+
+    for (const d of mdbAccountsPublicData.rows) {
+        q(
+            `INSERT INTO public (
+                id,
+                username,
+                usernameOld,
+                usernameOldExpire,
+                displayName,
+                fanflair,
+                avatar,
+                banner,
+                status,
+                about,
+                tags,
+                birthdate,
+                birthdateVisibility,
+                founded,
+                foundedVisibility,
+                theme,
+                isAuraEnabled,
+                auraType,
+                auraPrimary,
+                auraSecondary,
+                type,
+                explicit,
+                visibility,
+                recieveMessages,
+                lastActive,
+                lastActiveVisibility,
+                createdDate
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [
+                d.id,
+                d.username,
+                d.username_old ,
+                d.username_old_expire ,
+                d.display_name,
+                d.fanflair,
+                d.avatar,
+                d.banner,
+                d.status,
+                d.about,
+                d.tags,
+                d.birthdate,
+                d.birthdate_visibility,
+                d.founded,
+                d.founded_visibility,
+                d.theme,
+                d.aura || 0,
+                "flow",
+                d.aura_primary,
+                d.aura_secondary,
+                d.type,
+                d.explicit || 0,
+                d.visibility || "public",
+                d.messages,
+                d.last_active,
+                d.last_active_visibility,
+                d.created_date
+            ]
+        );
+    }
+});
+
+// accounts.db/badges -> badges.sqlite
+const mdbAccountsBadgesData = mdb.accounts.query("SELECT * from badges");
+
+db.badges.transaction(q => {
+    if (!mdbAccountsBadgesData.success) return;
+
+    for (const d of mdbAccountsBadgesData.rows) {
+        if (d.type === "admin" || d.type === "moderator") {
+            d.type = "staff";
+        }
+
+        q(
+            `INSERT INTO badges (
+                id,
+                type,
+                comment,
+                visibility,
+                date
+            ) VALUES (?, ?, ?, ?, ?)`,
+            [
+                d.user,
+                d.type,
+                d.text,
+                d.visibility,
+                d.date
+            ]
+        );
+    }
 });
 
 /* 
@@ -75,6 +256,7 @@ Routes
 app.use('/v1', v1);
 
 v1.use('/users', userRoutes);
+v1.use('/profiles', profileRoutes);
 
 /* 
 ————————————————————————————————————————————————————————————————
