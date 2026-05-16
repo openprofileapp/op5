dd6c1146f3678406db4639404c32f14d42f668bf
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-05-16 14:25:37 -0400

    updated imports

--- src/backend/api/routes/user.routes.ts
+++ src/backend/api/routes/user.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import { getUsers, getUser, createUser } from '../controllers/user.controller.js';
+import { getUsers } from '../controllers/user.controller.js';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getUsers);
-userRoutes.get('/:id', getUser);
-userRoutes.post('/create', createUser);
 
 export default userRoutes;
