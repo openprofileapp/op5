41245530936332f32ac61634a931017d05d6d63d
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-05-16 14:27:51 -0400

    updated cards

--- src/frontend/main/pages/Search.tsx
+++ src/frontend/main/pages/Search.tsx
@@ -31,11 +31,11 @@ export default function Search() {
 
                     <CharacterCard
                         id="6587823496323314"
-                        aura={{ start: "#e03c17", end: "#e03c17" }}
+                        aura={{ primary: "#e03c17", secondary: "#e03c17" }}
                         avatar="https://cdn.openprofile.app/uploads/profiles/6587823496323314/6587823496323314.png"
                         name="Mable Jackson"
                         slug="eclipse"
-                        owner={{ aura: { start: "#76d1ff", end: "#76d1ff" }, id: "5019646586243236", slug: "dragonights", avatar: "https://cdn.dragonights.com/r/dragonights_logo_512_png.png", name: "Dragonights", verified: true }}
+                        owner={{ aura: { primary: "#76d1ff", secondary: "#76d1ff" }, id: "5019646586243236", slug: "dragonights", avatar: "https://cdn.dragonights.com/r/dragonights_logo_512_png.png", name: "Dragonights", verified: true }}
                         overview="Mable Jackson, also known as Eclipse, is an American martial arts and hacking prodigy, associate nurse, and scientist specializing in dragonite research."
                         interactions={{ views: { count: 275, interacted: true }, likes: { count: 2, interacted: true } }}
                         notification={{ isActive: false, time: "5 minutes ago"}}
@@ -43,22 +43,22 @@ export default function Search() {
 
                     <CharacterCard
                         id="6690301862165288"
-                        aura={{ start: "#76d1ff", end: "#76d1ff" }}
+                        aura={{ primary: "#76d1ff", secondary: "#76d1ff" }}
                         avatar="https://cdn.openprofile.app/uploads/profiles/6690301862165288/nBbkedwiGNmsTZIvD470x3TYsw67AbIO.png"
                         name="AvatarKage"
                         slug="avatarkage"
-                        owner={{ aura: { start: "#76d1ff", end: "#76d1ff" }, id: "5019646586243236", slug: "dragonights", avatar: "https://cdn.dragonights.com/r/dragonights_logo_512_png.png", name: "Dragonights", verified: true }}
+                        owner={{ aura: { primary: "#76d1ff", secondary: "#76d1ff" }, id: "5019646586243236", slug: "dragonights", avatar: "https://cdn.dragonights.com/r/dragonights_logo_512_png.png", name: "Dragonights", verified: true }}
                         overview="AvatarKage, also known as Blue Knight, is an American digital jack-of-all-trades who specializes in community leadership, brand management, and AI development."
                         interactions={{ views: { count: 200, interacted: true }, likes: { count: 2, interacted: true } }}
                     />
 
                     <CharacterCard
                         id="1655391085225720"
-                        aura={{ start: "#00a700", end: "#00a700" }}
+                        aura={{ primary: "#00a700", secondary: "#00a700" }}
                         avatar="https://cdn.openprofile.app/uploads/profiles/1655391085225720/kPfDPqJDCQKsxdFiNxnzkMGdD3ctFzzi.png"
                         name="Julia Anderson"
                         slug="ash"
-                        owner={{ aura: { start: "#76d1ff", end: "#76d1ff" }, id: "5019646586243236", slug: "dragonights", avatar: "https://cdn.dragonights.com/r/dragonights_logo_512_png.png", name: "Dragonights", verified: true }}
+                        owner={{ aura: { primary: "#76d1ff", secondary: "#76d1ff" }, id: "5019646586243236", slug: "dragonights", avatar: "https://cdn.dragonights.com/r/dragonights_logo_512_png.png", name: "Dragonights", verified: true }}
                         overview="Julia Anderson, also known as Ash, is an American alternative goth patrol officer who specializes in a variety of weapons and deadly martial arts."
                         interactions={{ views: { count: 261, interacted: true }, likes: { count: 2, interacted: false } }}
                     />
