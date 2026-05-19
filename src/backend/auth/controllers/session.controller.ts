ac0bb28f8db0f95461c9cf8a876e582b4c59c459
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-05-19 14:48:28 -0400

    updated session.controller.ts

--- src/backend/auth/controllers/session.controller.ts
+++ src/backend/auth/controllers/session.controller.ts
@@ -1,7 +1,37 @@
 import type { Request, Response } from 'express';
+import validateSession from '../services/validateSession.service.js';
 
-export const getSession = (req: Request, res: Response) => {
-    return res.status(400).json({
-        error: 'Invalid id'
-    });
+import { AdvancedError } from 'kage-library';
+
+import { log } from '../server.js';
+
+/*
+// Guard
+// Variables
+let COCURRENT_SESSIONS: number = 0;
+
+// If more than the max users, return 503
+COCURRENT_SESSIONS++;
+
+if (COCURRENT_SESSIONS > config.maxSessions) {
+    throw new Error("Server is busy", { cause: { code: 503 } });
+}
+*/
+
+export const getSession = async (req: Request, res: Response) => {
+    try {
+        const response = await validateSession(req, res);
+
+        return res.status(200).json({
+            ...response
+        });
+
+    } catch (error) {
+        if (error instanceof AdvancedError) {
+            log.db.error(error.stack).save();
+            return res.status(error.code).json(error.message);
+        } else {
+            console.log("Unknown error:", error);
+        }
+    }
 };
