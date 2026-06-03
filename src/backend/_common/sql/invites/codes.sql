9efebdc45dd5bbf7b90282c8246cbb5200234071
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-03 00:52:10 -0400

    added invites/codes.sql

---
+++ src/backend/_common/sql/invites/codes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS codes (
+    owner TEXT NOT NULL,
+    code TEXT PRIMARY KEY,
+    usesLeft INTEGER DEFAULT 0,
+    isUnlimited INTEGER NOT NULL DEFAULT 0
+        CHECK (isUnlimited IN (0, 1)), 
+    isSuspended INTEGER NOT NULL DEFAULT 0
+        CHECK (isSuspended IN (0, 1)), 
+    createdDate TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
+);
