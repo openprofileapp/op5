acb2cb22432763c22f5b1469d67801af70c77567
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-04 02:38:11 -0400

    updated and renamed session.route.ts

--- src/backend/auth/routes/session.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getSession } from '../controllers/session.controller.js';
-
-const sessionRoutes = Router();
-
-sessionRoutes.get('/', getSession);
-sessionRoutes.post('/', getSession);
-
-export default sessionRoutes;
