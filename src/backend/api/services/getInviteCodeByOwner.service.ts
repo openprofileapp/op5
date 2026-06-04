50d8572212dbc8c8c9c240c1022dfc797cc7c003
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-04 02:36:48 -0400

    added getInviteCodeByOwner.service.ts

---
+++ src/backend/api/services/getInviteCodeByOwner.service.ts
@@ -0,0 +1,16 @@
+import { db } from "../server.js";
+
+export default function getInviteCodeByOwner(id?: string) {
+    const codeResult = db.invites.query("SELECT * FROM codes WHERE ownerId = ?", [id]);
+    if (!codeResult.success) return { message: "An error occurred while fetching invite code" }
+    if (codeResult.rowCount < 1) return { message: "Code not found" }
+    const usesResult = db.invites.query("SELECT * FROM uses WHERE code = ?", [codeResult.rows[0]?.code]);
+    if (!usesResult.success) return { message: "An error occurred while fetching invite uses" }
+
+    // ONLY SHOW USES IF OWNER
+
+    return {
+        ...codeResult.rows[0],
+        uses: usesResult.rowCount
+    };
+}
