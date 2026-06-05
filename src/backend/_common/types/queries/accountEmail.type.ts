c0ed191a9bc7fa5845cf758c81bb878bc41a7499
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-05 06:47:17 -0400

    added accountEmail.type.ts

---
+++ src/backend/_common/types/queries/accountEmail.type.ts
@@ -0,0 +1,9 @@
+export type AccountEmail = {
+    userId: string;
+    email: string;
+    isConfirmed: boolean;
+    isMfa: boolean;
+    isSubscribedToNewsletters: boolean;
+    isSubscribedToAccountNotifications: boolean;
+    isSubscribedToPromotionalMaterial: boolean;
+}
