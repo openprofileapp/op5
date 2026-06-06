db719712366afc127a75768cef11c1b383cc9e22
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-06 06:40:34 -0400

    renamed file

--- src/backend/api/services/getPublicUserByIdOrUsername.service.ts
+++ src/backend/api/services/getPublicUserByIdOrUsername.service.ts
@@ -1,11 +1,13 @@
 import { db } from "../server.js";
 
-export default function getPublicUserById(id?: string) {
-    const userResult = db.users.query("SELECT * FROM users WHERE id = ?", [id]);
-    const badgesResult = db.badges.query("SELECT * FROM badges WHERE id = ?", [id]);
-
+export default function getPublicUserByIdOrUsername(id?: string) {
+    const userResult = db.users.query("SELECT * FROM users WHERE id = ? OR username = ? OR usernameOld = ?", [id, id, id]);
+    
     if (!userResult.success) return { message: "An error occurred while fetching user" }
     if (userResult.rowCount < 1) return { message: "User not found" }
+
+    const badgesResult = db.badges.query("SELECT * FROM badges WHERE id = ?", [userResult.rows[0].id]);
+
     if (!badgesResult.success) return { message: "An error occurred while fetching badges" }
 
     return {
