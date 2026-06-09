5b4e9083aacddabc104cdc40aae672800988a606
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-09 09:32:22 -0400

    fixed toast classes compilation failing on restart

--- src/frontend/main/components/ToastContainer.tsx
+++ src/frontend/main/components/ToastContainer.tsx
@@ -9,6 +9,13 @@ type ToastUI = Toast & {
 export default function ToastContainer() {
     const [toasts, setToasts] = useState<ToastUI[]>([]);
 
+    const toastClasses: Record<Toast["type"], string> = {
+        info: "bg-base-200 border-base-300",
+        success: "bg-success text-success-content border-success",
+        warning: "bg-warning text-warning-content border-warning",
+        error: "bg-error text-error-content border-error",
+    };
+
     useEffect(() => {
         const unsubscribe = toast.subscribe((t) => {
             const id = t.id;
@@ -43,11 +50,8 @@ export default function ToastContainer() {
                         exit={{ opacity: 0, scale: 0.9, y: -5 }}
                         transition={{ duration: 0.2 }}
                         className={`
-                            alert
-                            ${t.type === "info"
-                                ? "bg-base-200 border-base-300"
-                                : `bg-${t.type} border-${t.type}`}
-                            origin-center
+                            alert origin-center
+                            ${toastClasses[t.type]}
                         `}
                     >
                         {t.icon && (<span className="flex items-center justify-center w-4 text-lg font-nerdfont leading-none shrink-0">{t.icon}</span>)}
