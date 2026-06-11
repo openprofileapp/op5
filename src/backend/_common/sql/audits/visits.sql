a9adf3054c0c82dbbae1ac1195b8c69ba3c068b7
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-11 06:54:44 -0400

    deleted visits.sql

--- src/backend/_common/sql/audits/visits.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS visits (
-    userId TEXT NOT NULL DEFAULT 'guest',
-    url TEXT NOT NULL,
-    duration INTEGER NOT NULL,
-    disconnected TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
-);
