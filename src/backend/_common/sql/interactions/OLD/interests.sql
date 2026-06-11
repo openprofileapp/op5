f5d3cfe77f856c9f175339ada06dd71f4da6baff
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-11 06:55:24 -0400

    deleted files

--- src/backend/_common/sql/interactions/OLD/interests.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS interests (
-    user TEXT NOT NULL,
-    interaction TEXT NOT NULL,
-    date DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
--- interests should be score based
--- src/backend/_common/sql/interactions/friendRequests.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS friendRequests (
-    sourceId TEXT NOT NULL, -- User id
-    targetId TEXT NOT NULL, -- User id
-    date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
-);
