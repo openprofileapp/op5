0241c02dfe5c8342701e994e9737e8dde4ab3db4
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-11 06:56:24 -0400

    updated interaction sql tables

--- src/backend/_common/sql/interactions/mutes.sql
+++ src/backend/_common/sql/interactions/mutes.sql
@@ -1,11 +1,10 @@
 CREATE TABLE IF NOT EXISTS mutes (
     sourceId TEXT NOT NULL, -- User id
     targetId TEXT NOT NULL, -- User or asset id
-    muteContent INTEGER NOT NULL DEFAULT 0
-        CHECK (muteContent IN (0, 1)),
-    muteInteractions INTEGER NOT NULL DEFAULT 0
-        CHECK (muteInteractions IN (0, 1)),
-    muteMessages INTEGER NOT NULL DEFAULT 0
-        CHECK (muteMessages IN (0, 1)),
-    date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
+    muteContent INTEGER NOT NULL DEFAULT 0,
+    muteInteractions INTEGER NOT NULL DEFAULT 0,
+    muteMessages INTEGER NOT NULL DEFAULT 0,
+    date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
+
+    UNIQUE (sourceId, targetId) -- Ensure source/target do not have multiple of the same interactions
 );
--- src/backend/_common/sql/interactions/views.sql
+++ src/backend/_common/sql/interactions/views.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS views (
     sourceId TEXT NOT NULL, -- User or socket id
     targetId TEXT NOT NULL, -- User or asset id
-    date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
+    date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
+
+    UNIQUE (sourceId, targetId) -- Ensure source/target do not have multiple of the same interactions
 );
