53b482327ef0e0a8c39a37ba56599d6795c2fafb
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-12 07:03:25 -0400

    updated interactions.controller.ts

--- src/backend/api/controllers/interactions.controller.ts
+++ src/backend/api/controllers/interactions.controller.ts
@@ -2,27 +2,281 @@ import type { Request, Response } from 'express';
 
 import { db } from '../server.js';
 
-export const getInteraction = (req: Request, res: Response) => {
-    const { interaction } = req.params;
-    const { source, target, page } = req.query;
+export const getInteractionsCount = (req: Request, res: Response) => {
+    const { userId } = req.params;
 
-    if (!source && !target) return res.status(400).json({ error: "Invalid interaction" });
+    // MOVE MOST OF THIS TO AN EXTERNAL SERVICE THEN CALL IT HERE
+    // RENAME TO /STATS OR SOMETHING
 
     // hasPermissionsToView()??
     // ^ isBlocked(); built-in
 
-    const conditions: string[] = [];
+    // ONLY INCLUDE BLOCKS/FRIENDS/MUTES/RESTRICTS AND STUFF IF ITS THE OWNER OF THE ACCOUNT/ASSET
 
-    if (source) conditions.push(`sourceId = ${source}`);
-    if (target) conditions.push(`targetId = ${target}`);
+    /* 
+    ————————————————————————————————————————————————————————————————
+    userProfileInteractionCounts
+    ———————————————————————————————————————————————————————————————— 
+    */
 
-    const result = db.interactions.query(`SELECT * FROM ${interaction} WHERE ${conditions.join(" AND ")}`);
+    // Following
+    const userProfileFollowingResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM follows WHERE sourceId = ?`, 
+        [userId]
+    );
 
-    if (!result.success) return res.status(400).json({ error: "An error occurred while fetching interaction" });
+    if (!userProfileFollowingResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching following" }
+    );
+
+    // Followers
+    const userProfileFollowersResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM follows WHERE targetId = ?`, 
+        [userId]
+    );
+
+    if (!userProfileFollowersResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching followers" }
+    );
+
+    // Shares
+    const userProfileSharesResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM shares WHERE targetId = ?`, 
+        [userId]
+    );
+
+    if (!userProfileSharesResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching shares" }
+    );
+
+    // Views
+    const userProfileViewsResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM views WHERE targetId = ?`, 
+        [userId]
+    );
+
+    if (!userProfileViewsResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching views" }
+    );
+
+    /* 
+    ————————————————————————————————————————————————————————————————
+    contentInteractionCounts
+    ———————————————————————————————————————————————————————————————— 
+    */
+
+    const characterIdsResult = db.characters.query(
+        "SELECT id FROM published WHERE ownerId = ?", 
+        [userId]
+    );
+
+    if (!characterIdsResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching characters" }
+    );
+    
+    const characterIds = (characterIdsResult.rows as { id: string }[]).map(c => c.id);
+    
+    // Followers
+    const contentFollowersResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM follows WHERE targetId IN (
+            ${characterIds.map(() => "?").join(",")}
+        )`, 
+        characterIds
+    );
+    
+    if (!contentFollowersResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching followers" }
+    );
+
+    // Likes
+    const contentLikesResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM likes WHERE targetId IN (
+            ${characterIds.map(() => "?").join(",")}
+        )`, 
+        characterIds
+    );
+    
+    if (!contentLikesResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching likes" }
+    );
+
+    // Reads
+    const contentReadsResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM reads WHERE targetId IN (
+            ${characterIds.map(() => "?").join(",")}
+        )`, 
+        characterIds
+    );
+    
+    if (!contentReadsResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching reads" }
+    );
+
+    // Shares
+    const contentSharesResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM shares WHERE targetId IN (
+            ${characterIds.map(() => "?").join(",")}
+        )`, 
+        characterIds
+    );
+    
+    if (!contentSharesResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching shares" }
+    );
+
+    console.log(characterIds)
+
+    // Views
+    const contentViewsResult = db.interactions.query(
+        `SELECT COUNT(*) AS count FROM views WHERE targetId IN (
+            ${characterIds.map(() => "?").join(",")}
+        )`, 
+        characterIds
+    );
+    
+    if (!contentViewsResult.success) return res.status(500).json(
+        { error: "An error occurred while fetching views" }
+    );
+
+    /* 
+    ————————————————————————————————————————————————————————————————
+    Response JSON
+    ———————————————————————————————————————————————————————————————— 
+    */
+
+    res.status(200).json({
+        userId,
+        userProfileInteractionCounts: {
+            following: userProfileFollowingResult.rows[0].count || 0,
+            followers: userProfileFollowersResult.rows[0].count || 0,
+            shares: userProfileSharesResult.rows[0].count || 0,
+            views: userProfileViewsResult.rows[0].count || 0
+        },
+        contentInteractionCounts: {
+            followers: contentFollowersResult.rows[0].count || 0,
+            likes: contentLikesResult.rows[0].count || 0,
+            reads: contentReadsResult.rows[0].count || 0,
+            shares: contentSharesResult.rows[0].count || 0,
+            views: contentViewsResult.rows[0].count || 0
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://api.openprofile.app/v2/interactions/5719552362357773/following?page=2 (list)
+// https://api.openprofile.app/v2/interactions/5719552362357773 (all interactions count)
+// Which gives only counts; eg 500 views etc.
+
+export const getFollowing = (req: Request, res: Response) => {
+    const { userId } = req.params;
+    const { page } = req.query;
+
+    // hasPermissionsToView()??
+    // ^ isBlocked(); built-in
+
+    const result = db.interactions.query(`SELECT * FROM follows WHERE sourceId = ?`, [userId]);
+
+    if (!result.success) return res.status(500).json({ error: "An error occurred while fetching following" });
     if (result.rowCount < 1) return res.status(404).json({ error: "Interaction not found" });
 
-    res.status(200).json(result.rows);
+    const following = result.rows.map((row) => ({ // Type goes at row
+        userId: row.targetId,
+        date: row.date
+    }));
+
+    res.status(200).json({
+        userId,
+        count: result.rowCount,
+        following
+    });
 };
+
+export const getFollowers = (req: Request, res: Response) => {
+    const { userId } = req.params;
+    const { page } = req.query;
+
+    // hasPermissionsToView()??
+    // ^ isBlocked(); built-in
+
+    const result = db.interactions.query(`SELECT * FROM follows WHERE targetId = ?`, [userId]);
+
+    if (!result.success) return res.status(500).json({ error: "An error occurred while fetching interaction" });
+    if (result.rowCount < 1) return res.status(404).json({ error: "Interaction not found" });
+
+    const followers = result.rows.map((row) => ({ // Type goes at row
+        userId: row.targetId,
+        date: row.date
+    }));
+
+    res.status(200).json({
+        userId,
+        count: result.rowCount,
+        followers
+    });
+};
+
+// DIFFERENT API
+// https://api.dev.openprofile.app/v2/relationship/5719552362357773/0000000000000000
+export const getRelationship = (req: Request, res: Response) => {
+    const { sourceUserId, targetUserId } = req.params;
+    const { page } = req.query;
+
+    // hasPermissionsToView()??
+    // ^ isBlocked(); built-in
+
+    const followsResult = db.interactions.query(
+        `SELECT * FROM follows WHERE sourceId = ? AND targetId = ?`, 
+        [sourceUserId, targetUserId]
+    );
+
+    const friendsResult = db.interactions.query(
+        `SELECT * FROM friends WHERE sourceId = ? AND targetId = ?`, 
+        [sourceUserId, targetUserId]
+    );
+
+    if (!followsResult.success && friendsResult.success) return res.status(500).json({ error: "An error occurred while fetching interaction" });
+    if (followsResult.rowCount < 1 && friendsResult.rowCount < 1) return res.status(404).json({ error: "Interaction not found" });
+
+    res.status(200).json({
+        follows: followsResult.rows,
+        friends: friendsResult.rows
+    });
+
+  //  {
+  //"sourceUserId": "5719552362357773",
+  //"targetUserId": "1844584278027570",
+ // "following": true,
+ /// "followedBy": false,
+//  "friends": false
+// blocked etc
+//}
+};
+
+
 
 export const postInteraction = (req: Request, res: Response) => {};
 
