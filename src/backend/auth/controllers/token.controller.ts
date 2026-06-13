479224908d1ec9d92ccaec77c271e6c9af852ffb
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-13 10:49:47 -0400

    added token.controller.ts

---
+++ src/backend/auth/controllers/token.controller.ts
@@ -0,0 +1,43 @@
+import type { Request, Response } from 'express';
+
+import { AdvancedError } from 'kage-library';
+
+import { db, log } from '../server.js';
+import { SessionType } from '../../_common/types/queries/session.type.js';
+import isBearerTokenAuthorized from '../../api/helpers/isBearerTokenAuthorized.js';
+
+export const isAccessTokenValid = async (req: Request, res: Response) => {
+    try {
+        if (!await isBearerTokenAuthorized(req.headers.authorization)) {
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+
+        const result = db.accounts.query<SessionType>(
+            `SELECT accessToken FROM sessions WHERE accessToken = ? LIMIT 1`,
+            [req.body.token]
+        );
+
+        if (!result.success) {
+            throw new AdvancedError({
+                code: 500,
+                message: "An error occurred while fetching access token",
+                details: result.error
+            })
+        }
+
+        return res.status(200).json(
+            { valid: result.rowCount > 0 }
+        );
+
+    } catch (error) {
+        if (error instanceof AdvancedError) {
+            log.db.error(error).save();
+            return res.status(error.code).json({
+                id: error.id,
+                message: error.message
+            });
+        } else {
+            log.unknown.error("Unknown error:", error).save();
+        }
+    } 
+};
