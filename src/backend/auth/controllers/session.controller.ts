049533ea27e00db8da89b60500882b68b5a0cde3
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-14 10:48:00 -0400

    updated session.controller.ts

--- src/backend/auth/controllers/session.controller.ts
+++ src/backend/auth/controllers/session.controller.ts
@@ -5,39 +5,20 @@ import { AdvancedError } from 'kage-library';
 
 import { log } from '../server.js';
 
-/*
-// Guard
-// Variables
-let COCURRENT_SESSIONS: number = 0;
-
-// If more than the max users, return 503
-COCURRENT_SESSIONS++;
-
-if (COCURRENT_SESSIONS > config.maxSessions) {
-    throw new Error("Server is busy", { cause: { code: 503 } });
-}
-*/
-
-export const getSession = async (req: Request, res: Response) => {
+export const authenticateSession = async (req: Request, res: Response) => {
     try {
+        // Validates and returns the session
         const response = await validateSession(req, res);
 
-        if (response.action) {
-            switch (response.action) {
-                case "REFRESH_PAGE":
-                    return res.redirect(
-                        req.originalUrl || "/"
-                    );
-                default: 
-                    return res.status(200).json(
-                        {...response}
-                    );
-            }
-        } else {
-            return res.status(200).json({
-                ...response
-            });
+        if (response.action === "REFRESH_PAGE") {
+            return res.redirect(
+                req.originalUrl || "/"
+            );
         }
+
+        return res.status(200).json(
+            {...response}
+        );
     } catch (error) {
         if (error instanceof AdvancedError) {
             log.db.error(error).save();
