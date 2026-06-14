bd432e5a91f6f1fd175b5130475b1de26724048a
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-14 10:45:33 -0400

    added audit.controller.ts

---
+++ src/backend/api/controllers/audit.controller.ts
@@ -0,0 +1,32 @@
+import type { Request, Response } from "express";
+
+import { AdvancedError } from "kage-library";
+
+import isBearerTokenAuthorized from "../helpers/isBearerTokenAuthorized.js";
+import createAuditLog from "../services/createAuditLog.service.js";
+import { log } from "../server.js";
+
+export const createAuditLogController = async (req: Request, res: Response) => {
+    try {
+        if (!await isBearerTokenAuthorized(req.headers.authorization)) {
+            
+            // CREATE ACCESS AUDIT REPORTS ON FALSE AUTHORIZED; ALL APIS
+
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+
+        createAuditLog(req.body);
+
+        return res.status(200).json({ ok: true });
+    } catch (error) {
+        if (error instanceof AdvancedError) {
+            log.db.error(error).save();
+            return res.status(error.code).json({
+                id: error.id,
+                message: error.message
+            });
+        } else {
+            log.unknown.error("Unknown error:", error).save();
+        }
+    }
+};
