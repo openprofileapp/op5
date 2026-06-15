98805acdc5c2a1a5abecdd0f7d75792544b4332e
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-15 09:27:25 -0400

    removed check and updated schema

--- src/backend/_common/sql/accounts/connections.sql
+++ src/backend/_common/sql/accounts/connections.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS connections (
     userId TEXT NOT NULL,
     connectionId TEXT PRIMARY KEY NOT NULL,
     connectionName TEXT NOT NULL,
-    isMfa INTEGER NOT NULL DEFAULT 0
-        CHECK (isMfa IN (0, 1)),
+    connectionText TEXT,
+    isMfa INTEGER NOT NULL DEFAULT 0,
     connectedDate TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
 );
