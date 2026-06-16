b0519d068e8f30d3157b57947b55377bb516d84d
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-16 10:49:21 -0400

    updated getUserAccountByExternalId.ts

--- src/backend/auth/services/getUserAccountByExternalId.service.ts
+++ src/backend/auth/services/getUserAccountByExternalId.service.ts
@@ -6,7 +6,7 @@ import { UserAccountType } from '../../_common/types/queries/userAccount.type.js
 import PlatformPermissionsService from '../../_common/services/platformPermissions.service.js';
 
 export default function getUserAccountByExternalId(service: string, id: string) {
-    const connectionResult = db.accounts.query(
+    const connectionResult = db.accounts.query<LoginConnectionType>(
         "SELECT * FROM connections WHERE connectionName = ? AND connectionId = ? LIMIT 1", 
         [service, id]
     );
@@ -14,14 +14,14 @@ export default function getUserAccountByExternalId(service: string, id: string) 
     if (connectionResult.success) {
         if (connectionResult.rowCount < 1) throw new AdvancedError({ code: 404, message: "Connection not found" });
 
-        const row = connectionResult.rows[0] as LoginConnectionType;
+        const row = connectionResult.rows[0];
 
-        const userResult = db.accounts.query("SELECT * FROM users WHERE id = ? LIMIT 1", [row.userId]);
+        const userResult = db.accounts.query<UserAccountType>("SELECT * FROM users WHERE id = ? LIMIT 1", [row.userId]);
 
         if (userResult.success) {
             if (userResult.rowCount < 1) throw new AdvancedError({ code: 404, message: "Account not found" });
 
-            const row = userResult.rows[0] as UserAccountType;
+            const row = userResult.rows[0]
 
             if (row.isDeleted) throw new AdvancedError({ code: 404, message: "Account not found" });
             if (row.isSuspended) throw new AdvancedError({ code: 403, message: "This account is suspended" });
