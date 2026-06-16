9815de6b732da22d57c7121cd6f14782a9110dc9
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-16 10:45:00 -0400

    added createMfaChallenge.ts

---
+++ src/backend/auth/helpers/createMfaChallenge.ts
@@ -0,0 +1,63 @@
+import { DateTime } from "luxon";
+import crypto from "crypto"
+
+import { AdvancedError } from "kage-library";
+
+import { db, id } from "../server.js";
+
+export function createMfaChallenge(secret: string, userId: string, sessionId: string) {
+    const mfaToken = id.gen("TOKEN");
+    const in30Days = DateTime.now().toUTC().plus({ days: 30 }).toISO();
+
+    const hashedMfaToken = crypto.createHash("sha256").update(mfaToken).digest("hex");
+
+    const sessionResult = db.accounts.query(
+        `UPDATE sessions SET 
+            mfaToken = ?, 
+            mfaTokenExpireDate = ?
+            WHERE sessionId = ? LIMIT 1`,
+        [
+            hashedMfaToken,
+            in30Days,
+            sessionId,
+        ]
+    );
+
+    if (!sessionResult.success) {
+        throw new AdvancedError({
+            code: 500,
+            message: "An error occurred while saving session token",
+            details: sessionResult.error
+        })
+    }
+
+    const in15Minutes = DateTime.now().toUTC().plus({ minutes: 15 }).toISO();
+
+    const challengeResult = db.accounts.query(
+        `INSERT INTO mfaChallenges (
+            mfaToken, 
+            userId,
+            expireDate
+        ) VALUES (?, ?, ?)`,
+        [
+            hashedMfaToken, 
+            userId,
+            in15Minutes
+        ]
+    );
+
+    if (!challengeResult.success) {
+        throw new AdvancedError({
+            code: 500,
+            message: "An error occurred while saving mfa challenge",
+            details: challengeResult.error
+        })
+    }
+
+    // ALSO ADD AUDIT LOGGING FOR A SUCESSFUL CHALLENGE CREATION
+
+    return {
+        type: "mfa",
+        value: mfaToken
+    };
+}
