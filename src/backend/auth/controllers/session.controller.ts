0c489ba23788122f1b93bdd97c199ebb53d7cbbe
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-18 07:08:00 -0400

    updated session.controller.ts

--- src/backend/auth/controllers/session.controller.ts
+++ src/backend/auth/controllers/session.controller.ts
@@ -1,33 +1,7 @@
 import type { Request, Response } from 'express';
-import validateSession from '../services/validateSession.service.js';
-
-import { AdvancedError } from 'kage-library';
-
-import { log } from '../server.js';
 
 export const authenticateSession = async (req: Request, res: Response) => {
-    try {
-        // Validates and returns the session
-        const response = await validateSession(req, res);
-
-        if (response.action === "REFRESH_PAGE") {
-            return res.redirect(
-                req.originalUrl || "/"
-            );
-        }
-
-        return res.status(200).json(
-            {...response}
-        );
-    } catch (error) {
-        if (error instanceof AdvancedError) {
-            log.db.error(error).save();
-            return res.status(error.code).json({
-                id: error.id,
-                message: error.message
-            });
-        } else {
-            log.unknown.error("Unknown error:", error).save();
-        }
-    } 
+    return res.status(200).json(
+        {...req.session}
+    );
 };
