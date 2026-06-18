484a101e0b11864da866e7a92534286f75bc4485
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-18 07:08:26 -0400

    added validateSession.middleware.ts

---
+++ src/backend/auth/middlewares/validateSession.middleware.ts
@@ -0,0 +1,36 @@
+import type { Request, Response, NextFunction } from 'express';
+
+import { AdvancedError } from 'kage-library';
+
+import { log } from '../instances.js';
+import validateSession from '../services/validateSession.service.js';
+
+export const validateSessionMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Validates and returns the session
+        const response = await validateSession(req, res);
+
+        console.log(response)
+
+        if (response.action === "REFRESH_PAGE") {
+            return res.redirect(
+                req.originalUrl || "/"
+            );
+        }
+
+        req.session = response;
+
+        next();
+    } catch (error) {
+        return;
+        if (error instanceof AdvancedError) {
+            log.db.error(error).save();
+            return res.status(error.code).json({
+                id: error.id,
+                message: error.message
+            });
+        } else {
+            log.unknown.error("Unknown error:", error).save();
+        }
+    } 
+};
