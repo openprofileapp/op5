9ba6fd9683f9463a7b816165af0bdd8178c8f39c
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-18 12:05:03 -0400

    added rate limits

---
+++ src/backend/api/databases/sql/audits/security/rateLimits.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS rateLimits (
+    id TEXT PRIMARY KEY NOT NULL,
+    source TEXT NOT NULL, -- User/asset/etc. id
+    target TEXT, -- User/asset/etc. id
+    action TEXT NOT NULL, -- ACTION_NAME, 
+    changes TEXT, -- JSON showing old and new { old: { ... }, new: { ... } }
+    origin TEXT NOT NULL DEFAULT 'unknown',
+    date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
+
+    UNIQUE (source, target, action, date) -- Ensure audit do not have multiple of the same logs
+);
