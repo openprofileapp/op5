b3fc3021c9a924906339fff6d88544b40b71ce72
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-18 13:31:07 -0400

    connected vite frontend to backend

--- src/backend/status/controllers/status.controller.ts
+++ src/backend/status/controllers/status.controller.ts
@@ -1,18 +1,22 @@
-import type { Request, Response } from 'express';
+import type { Request, Response } from "express"
+import path from "path"
+import fs from "fs"
 
-import { 
-    WebClient
-} from "kage-library";
+import { getReqUrl } from "kage-library";
 
-import { config } from '../../../../app.config.js';
+import { config } from "../../../../app.config.js"
+import { vite } from "../server.js"
+import { log } from "../instances.js"
 
-export const statusController = async (req: Request, res: Response) => {
-    const wc = new WebClient({
-        crawler: config.crawler,
-        useSecureSSL: config.isProduction
-    });
+export const renderApp = async (req: Request, res: Response) => {
+    const clientConfig = {
+        useNerdFonts: config.useNerdFonts,
+        theme: config.theme,
+        metadata: config.metadata,
+        domains: config.domains
+    }
 
-    // Do not ping the status server
+    /*// Do not ping the status server
     const api = await wc.ping(`https://${config.domains.api}`);
     const cdn = await wc.ping(`https://${config.domains.cdn}`);
     const main = await wc.ping(`https://${config.domains.main}`);
@@ -21,5 +25,27 @@ export const statusController = async (req: Request, res: Response) => {
         api, 
         cdn, 
         main 
-    });
-};
+    });*/
+
+    try {
+        const htmlPath = path.join(
+            config.folders.root, "src", "frontend", "status.html"
+        )
+
+        let html = fs.readFileSync(htmlPath, "utf-8")
+
+        html = html.replace(
+            "__CLIENT_CONFIG__", 
+            JSON.stringify(clientConfig)
+        )
+
+        if (vite) {
+            html = await vite.transformIndexHtml(getReqUrl(req), html)
+        }
+
+        res.status(200).set({ "Content-Type": "text/html" }).end(html)
+    } catch (error: unknown) {
+        log.client.error(error).save()
+        res.status(500).end("Internal Server Error")
+    }
+}
