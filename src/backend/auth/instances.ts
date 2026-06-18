62e245e370299255ab312dd7627a04122d1a7ead
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-18 12:05:15 -0400

    removed common instances

--- src/backend/auth/instances.ts
+++ src/backend/auth/instances.ts
@@ -1,7 +1,4 @@
-import path from "path";
-import maxmind, { CityResponse } from "maxmind";
-
-import { Identifier, Logger, Snowflake, WebClient } from "kage-library";
+import { Logger, Snowflake } from "kage-library";
 
 import { config } from "../../../app.config.js";
 
@@ -12,17 +9,3 @@ export const log = new Logger({
 });
 
 export const snowflake = new Snowflake(config.generation.epoch, 0);
-
-export const wc = new WebClient({
-    crawler: config.crawler,
-    useSecureSSL: config.isProduction
-});
-
-export const id = new Identifier({
-    HASH: { regex: /[a-f0-9]/, length: 32 },
-    TOKEN: { regex: /[A-Za-z0-9]/, length: 64 }
-});
-
-export const geoip2 = await maxmind.open<CityResponse>(
-    path.resolve("data/databases/static/geoip2/cities.mmdb")
-);
