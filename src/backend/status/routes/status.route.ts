a2564483c4b9ee192757a6bc8ffa58e888fb3e81
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-18 13:32:50 -0400

    connected vite frontend to backend

--- src/backend/status/routes/status.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { statusController } from '../controllers/status.controller.js';
-
-const router = Router();
-
-router.get('/', statusController);
-
-export default router;
