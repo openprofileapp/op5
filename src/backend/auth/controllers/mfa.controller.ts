e5e6576fc4c9c53cfd25eb080facf5ac9bb7d766
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-06-18 07:07:43 -0400

    added mfa.controller.ts

---
+++ src/backend/auth/controllers/mfa.controller.ts
@@ -0,0 +1,29 @@
+import type { Request, Response } from 'express';
+import getMfaMethods from '../services/getMfaMethods.service.js';
+
+import { AdvancedError } from 'kage-library';
+
+import { log } from '../instances.js';
+
+export const getMfaMethodsController = async (req: Request, res: Response) => {
+    try {
+        // req.session.userId
+        const userId = "0"
+
+        const response = await getMfaMethods(userId);
+
+        return res.status(200).json(
+            {...response}
+        );
+    } catch (error) {
+        if (error instanceof AdvancedError) {
+            log.db.error(error).save();
+            return res.status(error.code).json({
+                id: error.id,
+                message: error.message
+            });
+        } else {
+            log.unknown.error("Unknown error:", error).save();
+        }
+    } 
+};
