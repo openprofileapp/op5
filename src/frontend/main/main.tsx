863715eb9aeac8e3e60ef7e5b73aff9f2ce7cd21
AvatarKage <contact@avatarkage.com>
2026-07-11 00:55:34 -0400

    added dynamic font url

--- src/frontend/main/main.tsx
+++ src/frontend/main/main.tsx
@@ -10,6 +10,23 @@ import "../_common/styles/tailwind.css";
 import "../_common/styles/app.css"
 import "./scripts/main.js";
 
+const isGateway = window.location.hostname === window.config.domains.gateway;
+const style = document.createElement("style");
+
+style.textContent = `
+@font-face {
+    font-family: "Alexandria";
+    src: url("https://${isGateway ? window.config.domains.gateway : window.config.domains.cdn}${isGateway ? "/cdn" : ""}/fonts/alexandria/AlexandriaVariableFont.ttf") format("truetype");
+}
+
+@font-face {
+    font-family: "NerdFont";
+    src: url("https://${isGateway ? window.config.domains.gateway : window.config.domains.cdn}${isGateway ? "/cdn" : ""}/fonts/jetbrainsmono/JetBrainsMonoNerdFontPropo-Regular.ttf") format("truetype");
+}
+`;
+
+document.head.appendChild(style);
+
 // Have a display 503 scripts that re-renders the full page when recieving
 // { action: "DISPLAY_503" }
 
