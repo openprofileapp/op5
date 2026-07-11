b3a22241d52c5aefc91e3134aa74c6a901fc7766
AvatarKage <contact@avatarkage.com>
2026-07-10 22:11:39 -0400

    added user connection type

---
+++ src/backend/auth/types/userConnection.type.ts
@@ -0,0 +1,8 @@
+export type UserConnectionType = {
+    userId: string;
+    connectionId: string;
+    connectionName: string;
+    connectionText: string;
+    isMfa: boolean;
+    connectedDate: string;
+}
