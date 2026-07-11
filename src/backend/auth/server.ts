9ba0b6cea4ec08c079e82e2693eca9a084565922
AvatarKage <contact@avatarkage.com>
2026-07-11 04:39:05 -0400

    trimmed comments

--- src/backend/auth/server.ts
+++ src/backend/auth/server.ts
@@ -56,24 +56,6 @@ router.use("/session", validateSessionMiddleware, rateLimitMiddleware(240), sess
 router.use("/login", validateSessionMiddleware, rateLimitMiddleware(10), loginRoutes);
 router.use("/mfa", validateSessionMiddleware, rateLimitMiddleware(20), mfaRoutes);
 
-
-
-// login = creates the session 
-//
-// login() -> create session and stuff then calls validate()
-// validate() -> checks if valid else calls login()
-//
-// validate/session = quickly checks if the cookie is valid 
-
-// OLD CODE
-/*server.auth.post("/v2/session", watchdog, rate_limit(240), async (req, res) => {
-    const invite = req.query?.invite || req.cookies?.invite;
-    const { id, token } = req.body; // If bot login attempt
-    // try { } catch (error) {
-        forward_status("error", "server", "authenticate", error.code, error.message);
-        return res.status(500).send(error.message);
-    }*/
-
 /* 
 ————————————————————————————————————————————————————————————————
 Start server
@@ -102,15 +84,6 @@ cron.schedule("0 0 * * *", () => {
     log.cleanLogs();
 });
 
-
-
-
-
-
-
-
-
-
 /* 
 // CRON TO DELETE EXPIRED OR TERMINATED SESSIONS
 // CRON TO UNPREMIUM BADGE AND PERMS WHEN USER IS NO LONGER SUBSCRIBED (IF DATE EXISTS, ELSE SKIP)
@@ -127,15 +100,3 @@ if (!result.success) {
     })
 }
 */
-
-// PORT ACCOUNTS/USERS AND SESSION MANAGEMENT
-
-// Session management only applies to auth, all servers check-in with /session
-
-// https://auth.openprofile.app/session (kill active-session on country or state change)
-// https://auth.openprofile.app/login (both users and bots)
-
-// Add login with Microsoft, BlueSky, X, Facebook
-
-// auth manages the following db: sessions, users private
-// validateSession();
