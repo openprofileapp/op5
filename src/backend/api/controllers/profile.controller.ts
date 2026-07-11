71ce38d571a3babe8c72e5822ee10a368dc6f1fc
AvatarKage <contact@avatarkage.com>
2026-07-11 08:03:11 -0400

    added owner query

--- src/backend/api/controllers/profile.controller.ts
+++ src/backend/api/controllers/profile.controller.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from 'express';
 
 import getPublishedProfileById from '../services/getPublishedProfileById.service.js';
+import getPublishedProfileByOwner from '../services/getPublishedProfilesByOwner.service.js';
 import getPublishedProfiles from '../services/getPublishedProfiles.service.js';
 
 export const getProfiles = (req: Request, res: Response) => {
-    const { id, visibility } = req.query;
+    const { id, owner, visibility } = req.query;
 
     // MAYBE COMBIE FOR SMART FILTERING
 
@@ -18,6 +19,14 @@ export const getProfiles = (req: Request, res: Response) => {
         res.status(200).json({
             ...getPublishedProfileById(id)
         });
+    } else if (owner) {
+        if (typeof owner !== 'string') {
+            return res.status(400).json({
+                error: 'Invalid owner'
+            });
+        }
+
+        res.status(200).json(getPublishedProfileByOwner(owner));
     } else if (visibility) {
         if (typeof visibility !== 'string') {
             return res.status(400).json({
