486e88cf9030b788125251aba2fa8d8aa27b8540
AvatarKage <contact@avatarkage.com>
2026-07-14 04:27:47 -0400

    uncommented and updated fetch session

--- src/backend/api/middlewares/fetchSession.middleware.ts
+++ src/backend/api/middlewares/fetchSession.middleware.ts
@@ -1,15 +1,31 @@
-/*import type { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 
 import { AdvancedError } from "kage-library";
-import { config } from "../../../../app.config.js";
 
+import { config } from "../../../../app.config.js";
+import getEnv from "../../../_common/helpers/getEnv.js";
 import { log, wc } from "../instances.js";
 import { ValidSessionType } from "../../_common/types/validSession.type.js";
 
 export const fetchSessionMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const response = await wc.callAPI<ValidSessionType>(
-            `https://${config.domains.auth}/session`
+            `https://${config.domains.auth}/session`,
+            {
+                method: "POST",
+                auth: `ApiSecret ${getEnv("API_SECRET")}`,
+                body: {
+                    headers: req.headers,
+                    cookies: req.cookies,
+                    query: req.query,
+                    ip: req.ip,
+                    socket: {
+                        remoteAddress: req.socket.remoteAddress,
+                    },
+                    method: req.method,
+                    originalUrl: req.originalUrl,
+                },
+            }
         );
 
         req.session = response;
@@ -26,4 +42,4 @@ export const fetchSessionMiddleware = async (req: Request, res: Response, next: 
             log.unknown.error("Unknown error (fetchSession.middleware.ts):", error).save();
         }
     } 
-};*/
+};
