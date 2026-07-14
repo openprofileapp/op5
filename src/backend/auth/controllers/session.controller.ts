32b9944d76f48f30fac86af54c03d3099a018eaa
AvatarKage <contact@avatarkage.com>
2026-07-14 04:26:26 -0400

    deleted file

--- src/backend/auth/controllers/session.controller.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from 'express';
-
-export const authenticateSession = async (req: Request, res: Response) => {
-    return res.status(200).json(
-        {...req.session}
-    );
-};
