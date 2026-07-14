5e9fd6eca6d370afe46b14794828df1aa9484bb5
AvatarKage <contact@avatarkage.com>
2026-07-14 01:27:40 -0400

    updated pins sql

--- src/backend/api/databases/sql/users/pins.sql
+++ src/backend/api/databases/sql/users/pins.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS pins (
+    userId TEXT NOT NULL,
     assetId TEXT NOT NULL,
     position INTEGER DEFAULT 0,
-    note TEXT,
     date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
 );
