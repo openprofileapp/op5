6c1128fad70c9bea04bd9a55eda022e8ebed8559
AvatarKage <contact@avatarkage.com>
2026-07-14 05:00:56 -0400

    ' -> "

--- src/backend/api/routes/user.routes.ts
+++ src/backend/api/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import type { Request, Response } from 'express';
+import type { Request, Response } from "express";
 
 import { getUsers } from "../controllers/user.controller.js";
 
