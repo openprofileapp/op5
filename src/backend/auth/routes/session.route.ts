c1115a1045c2e822f38ce11d8cff0653c6ea1201
AvatarKage <contact@avatarkage.com>
2026-07-14 04:29:22 -0400

    updated session route

--- src/backend/auth/routes/session.route.ts
+++ src/backend/auth/routes/session.route.ts
@@ -1,8 +1,45 @@
 import { Router } from "express";
-import { authenticateSession } from "../controllers/session.controller.js";
+import type { Request, Response } from "express";
+
+import { validateSessionMiddleware } from "../middlewares/validateSession.middleware.js";
+import validateSession from "../services/validateSession.service.js";
+import { AdvancedError } from "kage-library";
+import { log } from "../instances.js";
+import isBearerTokenAuthorized from "../../_common/helpers/isBearerTokenAuthorized.js";
 
 const sessionRoute = Router();
 
-sessionRoute.get("/", authenticateSession);
+sessionRoute.get("/", validateSessionMiddleware, async (req: Request, res: Response) => {
+    return res.status(200).json(
+        {...req.session}
+    );
+});
+
+sessionRoute.post("/", async (req: Request, res: Response) => {
+    try {
+        if (!await isBearerTokenAuthorized(req.headers.authorization)) {
+            return res.status(401).json({ error: "Unauthorized" });
+        }
+        
+        const response = await validateSession(
+            {
+                ...req.body,
+                get(name: string) {
+                    return this.headers?.[name.toLowerCase()];
+                }
+            },
+            res
+        );
+
+        return res.json({ ...response });
+    } catch (error) {
+        if (error instanceof AdvancedError) {
+            log.network.error(error.stack).save();
+            return res.status(error.code).json(error.message);
+        } else {
+            console.log("Unknown error (session.route.ts):", error);
+        }
+    }
+});
 
 export default sessionRoute;
