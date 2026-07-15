f84fed543cc59d023275363103f4ece508190abd
AvatarKage <contact@avatarkage.com>
2026-07-14 20:58:07 -0400

    added pin routes

---
+++ src/backend/api/routes/pin.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { getPins } from "../controllers/users/pins/getPins.controller.js";
+import { postPins } from "../controllers/users/pins/postPins.controller.js";
+
+const userRoute = Router();
+
+userRoute.get("/:ownerId", getPins)
+userRoute.post("/:ownerId", postPins)
+
+export default userRoute;
