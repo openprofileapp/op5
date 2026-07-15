2aad6d5c7ec5694eab47cc304bb5f07369509543
AvatarKage <contact@avatarkage.com>
2026-07-15 07:29:04 -0400

    added character type

---
+++ src/backend/_common/types/queries/character.type.ts
@@ -0,0 +1,22 @@
+export type CharacterType = {
+    id: string;
+    ownerId: string;
+    slug: string;
+    displayName: string;
+    avatar: string;
+    animatedAvatar: string;
+    banner: string;
+    about: string;
+    tags: string;
+    licenseId: string;
+    isAuraEnabled: boolean;
+    auraType: string;
+    auraPrimary: string;
+    auraSecondary: string;
+    isExplicit: boolean;
+    visibility: string;
+    sendComments: string;
+    isScheduled: boolean;
+    updatedDate: string;
+    createdDate: string;
+}
