ba5e0710408274aef0707fff1411e371d98269b1
AvatarKage <contact@avatarkage.com>
2026-07-14 20:58:55 -0400

    added getPinsByOwnerId service

---
+++ src/backend/api/services/getPinsByOwnerId.service.ts
@@ -0,0 +1,30 @@
+import { AdvancedError } from "kage-library";
+
+import { db } from "../databases/db.js";
+
+export default function getPinsByOwnerId(id: string) {
+    if (!id) {
+        throw new AdvancedError({
+            code: 400,
+            message: "Invalid id" // OR malformed request and assign as like a config or lang
+        })
+    }
+
+    const result = db.pins.query(
+        "SELECT * FROM pins WHERE ownerId = ?", 
+        [id]
+    );
+
+    if (!result.success) {
+        throw new AdvancedError({
+            code: 500,
+            message: "An error occurred while fetching pins",
+            details: result.error
+        })
+    }
+
+    return {
+        count: result.rowCount, // maybe have a count? Needs a response if no pins exist
+        ...result.rows
+    };
+}
