1c0afe4294be5702b757edf835fd7986dbf59807
AvatarKage <contact@avatarkage.com>
2026-07-14 20:58:17 -0400

    updated user routes

--- src/backend/api/routes/user.routes.ts
+++ src/backend/api/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
 
-import { getUsers } from "../controllers/user.controller.js";
+import { getUsers } from "../controllers/getUsers.controller.js";
 
 const userRoute = Router();
 
@@ -10,14 +10,5 @@ userRoute.get("/", getUsers); // DEV NOTE: Add a limit/search
 userRoute.get("/:userId", async (req: Request, res: Response) => {
     return res.status(400).json({ error: "Invalid parameter"});
 });
-
-userRoute.get("/:userId/pins", (req: Request, res: Response) => {
-    // REQUIRE ACCESS TOKEN
-    const { userId } = req.params;
-
-    return res.status(400).json({ ...req.session});
-});
-
-// userRoute.post("/:userId/pins", CODE HERE, NO EXT CALL);
 
 export default userRoute;
