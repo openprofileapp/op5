c25678f61654ee1f191d9b966a640af8f0f1baa6
AvatarKage <contact@avatarkage.com>
2026-07-15 07:29:28 -0400

    added temp comment block

--- src/backend/api/controllers/users/pins/getPins.controller.ts
+++ src/backend/api/controllers/users/pins/getPins.controller.ts
@@ -4,12 +4,34 @@ import { AdvancedError } from 'kage-library';
 
 import { log } from '../../../instances.js';
 import getPinsByOwnerId from '../../../services/getPinsByOwnerId.service.js';
+import PlatformPermissionsService from '../../../../_common/services/platformPermissions.service.js';
 
 export const getPins = (req: Request, res: Response) => {
     try {
         const { ownerId } = req.params;
 
-        res.status(200).json({
+        /*// TEMP (DELETE)
+        if (req.session) {
+            return res.status(200).json({
+                owned: PlatformPermissionsService.can(
+                    req.session, 
+                    ["VIEW"], // "VIEW_BIRTHDATE" etc. (if no, trim that part when fetching profiles)
+                    "6773794953695671"
+                ),
+                unownedButAllowed: PlatformPermissionsService.can(
+                    req.session, 
+                    ["VIEW", "READ"], 
+                    "1655391085225720"
+                ),
+                unowned: PlatformPermissionsService.can(
+                    req.session, 
+                    ["VIEW"], 
+                    "6587823496323314"
+                )
+            });
+        }*/
+
+        return res.status(200).json({
             ...getPinsByOwnerId(ownerId as string)
         });
     } catch(error) {
