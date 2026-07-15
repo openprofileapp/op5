a63b435d7688c5cd93cd224d8cd7a39d76258f19
AvatarKage <contact@avatarkage.com>
2026-07-14 20:58:39 -0400

    made code more readable

--- src/backend/api/services/getPublicUserByIdOrUsername.service.ts
+++ src/backend/api/services/getPublicUserByIdOrUsername.service.ts
@@ -1,24 +1,75 @@
+import { AdvancedError } from "kage-library";
+
 import { db } from "../databases/db.js";
 
 export default function getPublicUserByIdOrUsername(id?: string) {
-    const userResult = db.users.query("SELECT * FROM users WHERE id = ? OR username = ? OR usernameOld = ?", [id, id, id]);
+    const userResult = db.users.query(
+        "SELECT * FROM users WHERE id = ? OR username = ? OR usernameOld = ?", 
+        [id, id, id]
+    );
+
+    if (!userResult.success) {
+        throw new AdvancedError({
+            code: 500,
+            message: "An error occurred while fetching user",
+            details: userResult.error
+        })
+    }
+
+    if (userResult.rowCount < 1) {
+        throw new AdvancedError({
+            code: 404,
+            message: "User not found"
+        })
+    }
+  
+    const badgesResult = db.badges.query(
+        "SELECT * FROM badges WHERE id = ?", 
+        [userResult.rows[0].id]
+    );
+
+    if (!badgesResult.success) {
+        throw new AdvancedError({
+            code: 500,
+            message: "An error occurred while fetching badges",
+            details: badgesResult.error
+        })
+    }
+
+    const linksResult = db.links.query(
+        "SELECT * FROM links WHERE id = ?", 
+        [userResult.rows[0].id]
+    );
+
+    if (!linksResult.success) {
+        throw new AdvancedError({
+            code: 500,
+            message: "An error occurred while fetching links",
+            details: linksResult.error
+        })
+    }
+
+    const followingResult = db.interactions.query(
+        "SELECT * FROM follows WHERE source = ?", 
+        [userResult.rows[0].id]
+    );
+
+    const followersResult = db.interactions.query(
+        "SELECT * FROM follows WHERE target = ?", 
+        [userResult.rows[0].id]
+    );
+
+    if (!followingResult.success || !followersResult.success) {
+        throw new AdvancedError({
+            code: 500,
+            message: "An error occurred while fetching follows",
+            details: {
+            ...(!followingResult.success && { following: followingResult.error }),
+            ...(!followersResult.success && { followers: followersResult.error }),
+            }
+        })
+    }
     
-    if (!userResult.success) return { error: "An error occurred while fetching user" }
-    if (userResult.rowCount < 1) return { error: "User not found" }
-
-    const badgesResult = db.badges.query("SELECT * FROM badges WHERE id = ?", [userResult.rows[0].id]);
-
-    if (!badgesResult.success) return { error: "An error occurred while fetching badges" }
-
-    const linksResult = db.links.query("SELECT * FROM links WHERE id = ?", [userResult.rows[0].id]);
-
-    if (!linksResult.success) return { error: "An error occurred while fetching links" }
-
-    const followingResult = db.interactions.query("SELECT * FROM follows WHERE source = ?", [userResult.rows[0].id]);
-    const followersResult = db.interactions.query("SELECT * FROM follows WHERE target = ?", [userResult.rows[0].id]);
-
-    if (!followingResult.success || !followersResult.success) return { error: "An error occurred while fetching follows" }
-
     return {
         ...userResult.rows[0],
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
