798fc5aee2426f1e597e63d3c5ae22a3c2830768
AvatarKage <contact@avatarkage.com>
2026-07-16 02:45:05 -0400

    added studio analytics

--- src/frontend/studio/main.tsx
+++ src/frontend/studio/main.tsx
@@ -6,28 +6,68 @@ import { I18nextProvider } from "react-i18next"
 
 import i18n from "../_common/i18n.js"
 
+import isGateway from "../_common/helpers/isGateway.js"
+
 import "../_common/styles/tailwind.css";
-import "../_common/styles/app.css";
+import "../_common/styles/app.css"
 import "./scripts/main.js";
 
-import About from "./pages/About.js"
-import Home from "./pages/Home.js"
-import NotFound from "./pages/NotFound.js"
+const style = document.createElement("style");
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <HelmetProvider>
-            <I18nextProvider i18n={i18n}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        
-                        <Route path="/404" element={<NotFound />} />
-                        <Route path="*" element={<Navigate to="/404" replace />} />
-                    </Routes>
-                </BrowserRouter>
-            </I18nextProvider>
-        </HelmetProvider>
-    </React.StrictMode>
-)
+style.textContent = `
+@font-face {
+    font-family: "Alexandria";
+    src: url("https://${isGateway() ? window.location.host : window.config.domains.cdn}${isGateway() ? "/cdn" : ""}/fonts/alexandria/AlexandriaVariableFont.ttf") format("truetype");
+}
+
+@font-face {
+    font-family: "NerdFont";
+    src: url("https://${isGateway() ? window.location.host : window.config.domains.cdn}${isGateway() ? "/cdn" : ""}/fonts/jetbrainsmono/JetBrainsMonoNerdFontPropo-Regular.ttf") format("truetype");
+}
+`;
+
+document.head.appendChild(style);
+
+// Have a display 503 scripts that re-renders the full page when recieving
+// { action: "DISPLAY_503" }
+
+import ToastContainer from "../_common/components/ToastContainer.js"
+import CaptchaPortal from "../_common/components/CaptchaPortal.js"
+
+import Home from "./pages/Home.js"
+
+import ComingSoon from "../../_common/pages/ComingSoon.js"
+import NotFound from "../../_common/pages/NotFound.js"
+
+async function bootstrap() {
+    const response = await fetch(
+        `https://${window.config.domains.auth}/session`,
+        {
+            credentials: "include",
+        }
+    );
+
+    window.session = await response.json();
+
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+        <React.StrictMode>
+            <HelmetProvider>
+                <I18nextProvider i18n={i18n}>
+                    <BrowserRouter>
+                        <ToastContainer />
+                        <CaptchaPortal siteKey={window.config.integrations.hcaptcha} />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/dashboard" element={<ComingSoon />} />
+
+                            <Route path="/404" element={<NotFound />} />
+                            <Route path="*" element={<Navigate to="/404" replace />} />
+                        </Routes>
+                    </BrowserRouter>
+                </I18nextProvider>
+            </HelmetProvider>
+        </React.StrictMode>
+    )
+}
+
+bootstrap();
