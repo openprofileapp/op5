a82f528d8982aaee9a5ac8cf2010e250c43d0da4
AvatarKage <contact@avatarkage.com>
2026-07-16 21:50:52 -0400

    reverted main and fixed routes

--- src/frontend/studio/main.tsx
+++ src/frontend/studio/main.tsx
@@ -1,5 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { HelmetProvider } from "react-helmet-async"
+import { I18nextProvider } from "react-i18next"
+
+import i18n from "../_common/i18n.js"
 
 import isGateway from "../_common/helpers/isGateway.js"
 
@@ -26,7 +31,17 @@ document.head.appendChild(style);
 // Have a display 503 scripts that re-renders the full page when recieving
 // { action: "DISPLAY_503" }
 
-import Layout from "./Layout.js";
+import Layout from "./Layout.js"
+import ToastContainer from "../_common/components/ToastContainer.js"
+import CaptchaPortal from "../_common/components/CaptchaPortal.js"
+import AskAlice from "../_common/components/AskAlice.js"
+
+import Dashboard from "./pages/Dashboard.js"
+import Analytics from "./pages/Analytics.js"
+import CharacterProfile from "./components/CharacterProfile.js"
+
+import ComingSoon from "../_common/pages/ComingSoon.js"
+import NotFound from "../_common/pages/NotFound.js"
 
 async function bootstrap() {
     const response = await fetch(
@@ -40,9 +55,30 @@ async function bootstrap() {
 
     ReactDOM.createRoot(document.getElementById("root")!).render(
         <React.StrictMode>
-            <Layout />
+            <HelmetProvider>
+                <I18nextProvider i18n={i18n}>
+                    <BrowserRouter>
+                        <ToastContainer />
+                        <CaptchaPortal siteKey={window.config.integrations.hcaptcha} />
+                        <AskAlice />
+                        <Routes>
+                            <Route path="/character" element={<CharacterProfile />} />
+
+                            <Route element={<Layout />}>
+                                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                                <Route path="/dashboard" element={<Dashboard />} />
+                                <Route path="/analytics" element={<Analytics />} />
+                                <Route path="/content" element={<ComingSoon />} />
+
+                                <Route path="/404" element={<NotFound />} />
+                                <Route path="*" element={<Navigate to="/404" replace />} />
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                </I18nextProvider>
+            </HelmetProvider>
         </React.StrictMode>
-    );
+    )
 }
 
 bootstrap();
