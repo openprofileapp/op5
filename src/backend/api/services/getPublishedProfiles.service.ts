cd08e92e2f26a1724ab06c40bc51f851c9f0294a
AvatarKage <contact@avatarkage.com>
2026-08-19 03:29:38 -0400

    added comments

--- src/backend/api/services/getPublishedProfiles.service.ts
+++ src/backend/api/services/getPublishedProfiles.service.ts
@@ -1,13 +1,25 @@
+import { ValidSessionType } from "../../../_common/types/validSession.type.js";
 import { db } from "../databases/db.js";
 import getPublicUserById from "./getPublicUserByIdOrUsername.service.js";
 
-export default function getPublishedProfiles(visibility: string = "public") {
-    const result = db.characters.query("SELECT * FROM published WHERE visibility = ?", [visibility]);
+export default function getPublishedProfiles(visibility: string = "public", session: ValidSessionType) {
+    // If not visibility throw a malformed request
+
+    let result;
+
+    if (session.userId) {
+        // Get session user interests here
+
+        // Apply them below with a 20 limit or smth
+        result = db.characters.query("SELECT * FROM published WHERE visibility = ?", [visibility]);
+    } else {
+        result = db.characters.query("SELECT * FROM published WHERE visibility = ?", [visibility]);
+    }
 
     if (!result.success) return { error: "An error occurred while fetching profiles" }
     if (result.rowCount < 1) return { error: "No profiles found" }
 
-    // CALL THE VISIBILITY FUNCTION TO DETERMINE IF THE USER CAN VIEW DATA
+    // DEVELOPER NEEDED: CALL THE VISIBILITY FUNCTION TO DETERMINE IF THE USER CAN VIEW DATA
     // visibility: owner.visibility
 
     const profiles = result.rows.map((d) => {
