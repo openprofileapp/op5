b07e1d5141d3cf0e879c0ca179ad12d620d00fcb
AvatarKage <contact@avatarkage.com>
2026-08-19 08:40:34 -0400

    removed optional global session type

--- src/backend/_common/types/express.d.ts
+++ src/backend/_common/types/express.d.ts
@@ -5,7 +5,7 @@ import { ValidSessionType } from "../../../_common/types/validSession.type.ts"
 declare global {
     namespace Express {
         interface Request {
-            session?: ValidSessionType;
+            session: ValidSessionType;
         }
     }
 }
