8d25540f1106892c170462d963446e91abe45f05
AvatarKage <contact@avatarkage.com>
2026-08-20 03:16:11 -0400

    updated character routes

--- src/backend/api/routes/character.routes.ts
+++ src/backend/api/routes/character.routes.ts
@@ -1,25 +1,23 @@
 import { Router } from "express";
 
-import { getCharacters } from "../controllers/character.controller.js";
-import { getRecentFollowingCharacters } from "../controllers/characters/getRecentFollowingCharacters.controller.js";
+import { getCharacters } from "../controllers/characters/getCharacters.controller.js";
+import { getPopularCharacters } from "../controllers/characters/getPopularCharacters.controller.js";
 import { getRecommendedCharacters } from "../controllers/characters/getRecommendedCharacters.controller.js";
 import { getRecommendedTaggedCharacters } from "../controllers/characters/getRecommendedTaggedCharacters.controller.js";
-import { getPopularCharacters } from "../controllers/characters/getPopularCharacters.controller.js";
 import { getTrendingCharacters } from "../controllers/characters/getTrendingCharacters.controller.js";
 import { getRecentCharacters } from "../controllers/characters/getRecentCharacters.controller.js";
+import { getRecentFollowingCharacters } from "../controllers/characters/getRecentFollowingCharacters.controller.js";
 import { getTaggedCharacters } from "../controllers/characters/getTaggedCharacters.controller.js";
 
 const characterRoutes = Router();
 
-// DEVELOPER NEEDED: Add "page" query which has 30 items per
-
-characterRoutes.get("/", getCharacters);
-characterRoutes.get("/recent/following", getRecentFollowingCharacters);
+characterRoutes.get("/", getCharacters); // Make it tailored to preferences like recommended
+characterRoutes.get("/popular", getPopularCharacters);
 characterRoutes.get("/recommended", getRecommendedCharacters);
 characterRoutes.get("/recommended/:tag", getRecommendedTaggedCharacters);
-characterRoutes.get("/popular", getPopularCharacters);
 characterRoutes.get("/trending", getTrendingCharacters);
 characterRoutes.get("/recent", getRecentCharacters);
-characterRoutes.get("/tag/:tag", getTaggedCharacters);
+characterRoutes.get("/recent/following", getRecentFollowingCharacters);
+characterRoutes.get("/tag/:tag", getTaggedCharacters); // Make it tailored to preferences like recommended
 
 export default characterRoutes;
