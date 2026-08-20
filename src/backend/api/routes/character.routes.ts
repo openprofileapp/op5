218ef53bc464392c3e9fb22e33e0d19c0e5daf70
AvatarKage <contact@avatarkage.com>
2026-08-19 20:52:40 -0400

    updated character routes

--- src/backend/api/routes/character.routes.ts
+++ src/backend/api/routes/character.routes.ts
@@ -1,19 +1,25 @@
 import { Router } from "express";
 
 import { getCharacters } from "../controllers/character.controller.js";
+import { getRecentFollowingCharacters } from "../controllers/characters/getRecentFollowingCharacters.controller.js";
 import { getRecommendedCharacters } from "../controllers/characters/getRecommendedCharacters.controller.js";
+import { getRecommendedTaggedCharacters } from "../controllers/characters/getRecommendedTaggedCharacters.controller.js";
 import { getPopularCharacters } from "../controllers/characters/getPopularCharacters.controller.js";
+import { getTrendingCharacters } from "../controllers/characters/getTrendingCharacters.controller.js";
+import { getRecentCharacters } from "../controllers/characters/getRecentCharacters.controller.js";
+import { getTaggedCharacters } from "../controllers/characters/getTaggedCharacters.controller.js";
 
 const characterRoutes = Router();
 
 // DEVELOPER NEEDED: Add "page" query which has 30 items per
 
-// On front page, replace popular with trending; trending is top 100 with the most interactions within the past 24 hours
-// Add "getTrendingCharacters"
 characterRoutes.get("/", getCharacters);
+characterRoutes.get("/recent/following", getRecentFollowingCharacters);
 characterRoutes.get("/recommended", getRecommendedCharacters);
+characterRoutes.get("/recommended/:tag", getRecommendedTaggedCharacters);
 characterRoutes.get("/popular", getPopularCharacters);
-characterRoutes.get("/trending", getCharacters);
-characterRoutes.get("/tag/:tag", getCharacters);
+characterRoutes.get("/trending", getTrendingCharacters);
+characterRoutes.get("/recent", getRecentCharacters);
+characterRoutes.get("/tag/:tag", getTaggedCharacters);
 
 export default characterRoutes;
