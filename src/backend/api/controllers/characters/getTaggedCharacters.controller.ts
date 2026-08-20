a0a1ad12c40cb693024aab24e10aa5ee98e37e45
AvatarKage <contact@avatarkage.com>
2026-08-19 20:52:21 -0400

    added getTaggedCharacters

---
+++ src/backend/api/controllers/characters/getTaggedCharacters.controller.ts
@@ -0,0 +1,80 @@
+import type { Request, Response } from "express";
+
+import { AdvancedError } from "kage-library";
+
+import { log } from "../../instances.js";
+import { db } from "../../databases/db.js";
+import getPublicUserByIdOrUsername from "../../services/getPublicUserByIdOrUsername.service.js";
+import { CharacterType } from "../../../../_common/types/queries/character.type.js";
+
+export const getTaggedCharacters = (req: Request, res: Response) => {
+    try {
+        const { id, owner, visibility = "public" } = req.query;
+        const { tag } = req.params;
+
+        const tagClause = tag ? `AND tags LIKE ?` : "";
+        const tagParams = tag ? [`%"${tag}"%`] : [];
+
+        const idClause = id ? "AND id = ?" : "";
+        const idParams = id ? [id] : [];
+
+        const ownerIdClause = owner ? "AND ownerId != ?" : "";
+        const ownerIdArgs = owner ? [owner] : [];
+
+        const result = db.characters.query<CharacterType>(
+            `
+                SELECT * FROM published
+                WHERE visibility = ?
+                    ${tagClause}
+                    ${idClause}
+                    ${ownerIdClause}
+                ORDER BY algorithmScore DESC 
+                LIMIT 30
+            `,
+            [
+                visibility,
+                ...tagParams,
+                ...idParams,
+                ...ownerIdArgs,
+            ]
+        );
+
+        if (!result.success) {
+            throw new AdvancedError({
+                code: 500,
+                message: "An error occurred while fetching characters",
+                details: result.error
+            })
+        }
+
+        const characters = result.rows.map((d) => {
+            const owner = getPublicUserByIdOrUsername(d.ownerId);
+
+            return {
+                ...d,
+
+                owner: owner
+                    ? {
+                        id: owner.id,
+                        username: owner.username,
+                        displayName: owner.displayName,
+                        badges: owner.badges,
+                        type: owner.type
+                    }
+                    : null
+            };
+        });
+
+        res.status(200).json(characters);
+    } catch(error) {
+        if (error instanceof AdvancedError) {
+            log.db.error(error).save();
+            return res.status(error.code).json({
+                id: error.id,
+                message: error.message
+            });
+        } else {
+            log.unknown.error(error).save();
+        }
+    }
+};
