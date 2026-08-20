5bd685bb243a45dbcfd21e458f48f69a40610d13
AvatarKage <contact@avatarkage.com>
2026-08-19 20:52:29 -0400

    added getTrendingCharacters

---
+++ src/backend/api/controllers/characters/getTrendingCharacters.controller.ts
@@ -0,0 +1,113 @@
+import type { Request, Response } from "express";
+
+import { AdvancedError } from "kage-library";
+
+import { log } from "../../instances.js";
+import { db } from "../../databases/db.js";
+import getPublicUserByIdOrUsername from "../../services/getPublicUserByIdOrUsername.service.js";
+import { CharacterType } from "../../../../_common/types/queries/character.type.js";
+import { InteractionType } from "../../../../_common/types/queries/interaction.type.js";
+
+export const getTrendingCharacters = (req: Request, res: Response) => {
+    try {
+        const { id, owner, visibility = "public" } = req.query;
+
+        const interactionsResult = db.interactions.query<InteractionType>(
+            `
+                SELECT target, COUNT(*) as count
+                FROM (
+                    SELECT target FROM follows WHERE date >= strftime('%Y-%m-%dT%H:%M:%fZ', 'now', '-1 day') 
+                    UNION ALL
+                    SELECT target FROM likes WHERE date >= strftime('%Y-%m-%dT%H:%M:%fZ', 'now', '-1 day') 
+                    UNION ALL
+                    SELECT target FROM reads WHERE date >= strftime('%Y-%m-%dT%H:%M:%fZ', 'now', '-1 day') 
+                    UNION ALL
+                    SELECT target FROM shares WHERE date >= strftime('%Y-%m-%dT%H:%M:%fZ', 'now', '-1 day') 
+                    UNION ALL
+                    SELECT target FROM views WHERE date >= strftime('%Y-%m-%dT%H:%M:%fZ', 'now', '-1 day')
+                )
+                GROUP BY target
+                ORDER BY count DESC
+                LIMIT 100
+            `
+        );
+
+        if (!interactionsResult.success) {
+            throw new AdvancedError({
+                code: 500,
+                message: "An error occurred while fetching interactions",
+                details: interactionsResult.error
+            })
+        }
+
+        const trendingTargetIds = interactionsResult.rows.map((row) => row.target);
+
+        if (trendingTargetIds.length === 0) {
+            return res.status(200).json([]);
+        }
+
+        const trendingClause = `AND id IN (${trendingTargetIds.map(() => "?").join(",")})`;
+
+        const idClause = id ? "AND id = ?" : "";
+        const idParams = id ? [id] : [];
+
+        const ownerIdClause = owner ? "AND ownerId != ?" : "";
+        const ownerIdArgs = owner ? [owner] : [];
+
+        const result = db.characters.query<CharacterType>(
+            `
+                SELECT * FROM published
+                WHERE visibility = ?
+                    ${trendingClause}
+                    ${idClause}
+                    ${ownerIdClause}
+                ORDER BY algorithmScore DESC 
+                LIMIT 30
+            `,
+            [
+                visibility,
+                ...trendingTargetIds,
+                ...idParams,
+                ...ownerIdArgs,
+            ]
+        );
+
+        if (!result.success) {
+            throw new AdvancedError({
+                code: 500,
+                message: "An error occurred while fetching characters",
+                details: result.error
+            })
+        }
+
+        const characters = result.rows.map((d) => {
+            const owner = getPublicUserByIdOrUsername(d.ownerId);
+
+            return {
+                ...d,
+
+                owner: owner
+                    ? {
+                        id: owner.id,
+                        username: owner.username,
+                        displayName: owner.displayName,
+                        badges: owner.badges,
+                        type: owner.type
+                    }
+                    : null
+            };
+        });
+
+        res.status(200).json(characters);
+    } catch(error) {
+        if (error instanceof AdvancedError) {
+            log.db.error(error).save();
+            return res.status(error.code).json({
+                id: error.id,
+                message: error.message
+            });
+        } else {
+            log.unknown.error(error).save();
+        }
+    }
+};
