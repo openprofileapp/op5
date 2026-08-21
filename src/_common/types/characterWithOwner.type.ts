7b75e059fc06066fef05fd7bd1842c7a242a5646
AvatarKage <contact@avatarkage.com>
2026-08-21 04:00:01 -0400

    added CharacterWithOwnerType

---
+++ src/_common/types/characterWithOwner.type.ts
@@ -0,0 +1,37 @@
+type BadgeType = {
+    type: string;
+    comment: string | null;
+    visibility: string;
+    date: string;
+};
+
+export type OwnerType = {
+    id: string;
+    username: string;
+    displayName: string;
+    badges: BadgeType[];
+    type: string;
+}
+
+export type CharacterWithOwnerType = {
+    id: string;
+    owner: OwnerType;
+    slug: string;
+    displayName: string;
+    avatar: string;
+    animatedAvatar: string;
+    banner: string;
+    about: string;
+    tags: string;
+    licenseId: string;
+    isAuraEnabled: boolean;
+    auraType: string;
+    auraPrimary: string;
+    auraSecondary: string;
+    isExplicit: boolean;
+    visibility: string;
+    sendComments: string;
+    isScheduled: boolean;
+    updatedDate: string;
+    createdDate: string;
+}
