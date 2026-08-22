0e30d869d6c1da76a73f25ea316b1105ac7ba7d5
AvatarKage <contact@avatarkage.com>
2026-08-22 11:34:14 -0400

    polished getBadgesById

--- src/backend/api/services/getBadgesById.service.ts
+++ src/backend/api/services/getBadgesById.service.ts
@@ -1,13 +1,32 @@
 import { AdvancedError } from "kage-library";
 
 import { db } from "../databases/db.js";
-import { BadgeType } from "../../../_common/types/queries/badge.type.js";
-import { GetBadgeType } from "../../../_common/types/getBadge.type.js";
+import { BadgeType, GetBadgeType } from "../../../_common/types/badge.type.js";
+import { buildSqlInClause } from "../../../_common/helpers/sql.js";
 
-export default function getBadgesById(id: string): GetBadgeType[]  {
+export default function getBadgesById(
+    id: string
+): GetBadgeType[];
+
+export default function getBadgesById(
+    ids: string[]
+): Record<string, GetBadgeType[]>;
+
+export default function getBadgesById(
+    ids: string | string[]
+): GetBadgeType[] | Record<string, GetBadgeType[]> {
+    const isArray = Array.isArray(ids);
+    const array = isArray ? ids : [ids];
+
+    if (array.length === 0) {
+        return isArray ? {} : [];
+    }
+
+    const { clause, params } = buildSqlInClause("id", array);
+
     const result = db.badges.query<BadgeType>(
-        "SELECT * FROM badges WHERE id = ?", 
-        [id]
+        `SELECT * FROM badges WHERE ${clause}`, 
+        params
     );
 
     if (!result.success) {
@@ -18,6 +37,23 @@ export default function getBadgesById(id: string): GetBadgeType[]  {
         });
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    return result.rows.map(({ id, ...badge }) => badge);
+    if (!isArray) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        return result.rows.map(({ id, ...rest }) => rest);
+    }
+
+    const data: Record<string, GetBadgeType[]> = {};
+
+    for (const id of array) {
+        data[id] = [];
+    }
+
+    for (const row of result.rows) {
+        const { id, ...rest } = row;
+        if (data[id]) {
+            data[id].push(rest);
+        }
+    }
+
+    return data;
 }
