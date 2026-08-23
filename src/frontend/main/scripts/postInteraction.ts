d48c863b61b42180c9a5ed9f2cab1a14f6a55250
AvatarKage <contact@avatarkage.com>
2026-08-23 09:46:58 -0400

    moved postInteraction to its own script

---
+++ src/frontend/main/scripts/postInteraction.ts
@@ -0,0 +1,23 @@
+import { InteractionNameType } from "../../../_common/types/interaction.type.js";
+import { apiHost } from "./hosts.js";
+
+export async function postInteraction(
+    targetId: string, 
+    type: InteractionNameType
+) {
+    try {
+        const response = await fetch(
+            `${apiHost}/v2/interactions`, 
+            { 
+                credentials: "include", 
+                method: "POST", 
+                headers: {"Content-Type": "application/json"}, 
+                body: JSON.stringify({ targetId, type })
+            }
+        );
+
+        return await response.json()
+    } catch (error) {
+        console.error('Failed to post interaction:', error);
+    }
+}
