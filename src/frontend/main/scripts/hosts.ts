fda14b8543129017d88c8914482c2f4122406cf0
AvatarKage <contact@avatarkage.com>
2026-08-23 09:47:11 -0400

    added studioHost

--- src/frontend/main/scripts/hosts.ts
+++ src/frontend/main/scripts/hosts.ts
@@ -1,7 +1,15 @@
 import isGateway from "../../_common/helpers/isGateway.js";
 
+const protocal = "https://";
+
 export const apiHost = [
-    "https://",
+    protocal,
     isGateway() ? window.location.host : window.config.domains.api,
     isGateway() ? "/api" : ""
+].join("");
+
+export const studioHost = [
+    protocal,
+    isGateway() ? window.location.host : window.config.domains.studio,
+    isGateway() ? "/studio" : ""
 ].join("");
