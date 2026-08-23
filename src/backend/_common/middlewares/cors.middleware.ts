6abbf8d3c088e9f83b75a825a6517551c15c7ed9
AvatarKage <contact@avatarkage.com>
2026-08-23 05:55:06 -0400

    added null support to cors

--- src/backend/_common/middlewares/cors.middleware.ts
+++ src/backend/_common/middlewares/cors.middleware.ts
@@ -16,7 +16,18 @@ export const corsMiddleware = cors({
             return callback(null, true);
         }
 
+        if (origin === "null") {
+            if (!config.isProduction) {
+                return callback(null, true);
+            }
+        }
+
+        // DEVELOPER NEED: Save this to audits
+        console.error(`CORS Blocked Origin: "${origin}"`);
+
         return callback(new Error("Not allowed by CORS"));
     },
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 });
