9f247ae3c04e24201c5b41212a457fb8388b5608
AvatarKage <contact@avatarkage.com>
2026-08-24 01:52:17 -0400

    added isPrimary

--- src/backend/auth/databases/migration/accounts/emails.db.migration.ts
+++ src/backend/auth/databases/migration/accounts/emails.db.migration.ts
@@ -1,33 +1,46 @@
+import { DateTime } from "luxon";
 import { log } from "../../../instances.js";
 import { db, mdb } from "../../db.js";
 
-const result = mdb.accounts.query("SELECT * from emails");
+const emailResult = mdb.accounts.query("SELECT * from emails");
+const privateResult = mdb.accounts.query("SELECT * from private");
 
 db.accounts.transaction(q => {
-    if (!result.success) return log.db.error(result.error).save();
+    if (!emailResult.success) return log.db.error(emailResult.error).save();
+    if (!privateResult.success) return log.db.error(privateResult.error).save();
 
-    for (const d of result.rows) {
-        const result = q(
+    const privateMap = new Map(
+        privateResult.rows.map(p => [p.id, p.created_date])
+    );
+
+    for (const d of emailResult.rows) {
+        const addedDate = privateMap.get(d.user);
+
+        const emailResult = q(
             `INSERT INTO emails (
                 userId,
                 email,
+                isPrimary,
                 isVerified,
                 isMfa,
                 isSubscribedToNewsletters,
                 isSubscribedToAccountNotifications,
-                isSubscribedToPromotionalMaterial
-            ) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                isSubscribedToPromotionalMaterial,
+                addedDate
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
             [
                 d.user,
                 d.email,
+                1,
                 d.confirmed,
                 d.mfa_enabled,
                 d.newsletter_updates,
                 d.newsletter_notifications,
-                d.newsletter_promotional
+                d.newsletter_promotional,
+                DateTime.fromSQL(addedDate as string, { zone: "utc" }).toISO()
             ]
         );
 
-        if (!result.success) return log.db.error(result.error).save();
+        if (!emailResult.success) return log.db.error(emailResult.error).save();
     }
 });
--- src/backend/auth/databases/sql/accounts/emails.sql
+++ src/backend/auth/databases/sql/accounts/emails.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS emails (
     userId TEXT NOT NULL,
     email TEXT PRIMARY KEY NOT NULL,
+    isPrimary INTEGER NOT NULL DEFAULT 0,
     isVerified INTEGER NOT NULL DEFAULT 0,
     isMfa INTEGER NOT NULL DEFAULT 0,
     isSubscribedToNewsletters INTEGER NOT NULL DEFAULT 1,
     isSubscribedToAccountNotifications INTEGER NOT NULL DEFAULT 1,
-    isSubscribedToPromotionalMaterial INTEGER NOT NULL DEFAULT 1
+    isSubscribedToPromotionalMaterial INTEGER NOT NULL DEFAULT 1,
+    addedDate TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
+
+    UNIQUE (userId, isPrimary)
 );
