0edc7a0c8394fd44edf7a3d684f29572938442e7
AvatarKage <contact@avatarkage.com>
2026-08-25 10:36:31 -0400

    added back AuditType

---
+++ src/_common/types/audit.type.ts
@@ -0,0 +1,40 @@
+export type AuditNameType = 
+    | "authentications"
+    | "cors"
+    | "rateLimits"
+    | "blocks"
+    | "chats"
+    | "follows"
+    | "friends"
+    | "hiddenCollaborations"
+    | "hides"
+    | "likes"
+    | "mutes"
+    | "reads"
+    | "restricts"
+    | "shares"
+    | "views";
+
+type ChangesType = {
+    new: object;
+    old: object;
+}
+
+export type AuditType = {
+    id: string;
+    source: string;
+    target: string;
+    action: string;
+    changes: ChangesType;
+    origin: string;
+    date: string;
+}
+
+export type AuditApiType = {
+    type: string;
+    source: string | object;
+    target?: string | object;
+    action: string;
+    changes?: ChangesType;
+    origin?: string;
+}
