1c1b42cd8ea78831a68aaa2d063a6619e57f417f
AvatarKage <contact@avatarkage.com>
2026-08-25 03:49:17 -0400

    polished assert permissions

---
+++ src/backend/_common/asserts/assetPermissions.assert.ts
@@ -0,0 +1,25 @@
+import { AdvancedError } from "kage-library";
+import AssetPermissionsService, { AssetPermissionName } from "../../api/services/assetPermissions.service.js";
+
+/**
+ * Asserts that the current session must have the requested permissions.
+ *
+ * @example
+ * assertPlatformPermissions(req.session, "VIEW");
+ */
+export function assertAssetPermissions(
+    userId: string, 
+    requiredPermission: AssetPermissionName | AssetPermissionName[],
+    assetId: string
+): void {
+    if (!AssetPermissionsService.can(
+        userId, 
+        requiredPermission,
+        assetId
+    )) {
+        throw new AdvancedError({
+            code: 401,
+            message: "Unauthorized"
+        })
+    }
+}
---
+++ src/backend/_common/asserts/platformPermissions.assert.ts
@@ -0,0 +1,24 @@
+import { AdvancedError } from "kage-library";
+import { ValidSessionType } from "../../../_common/types/validSession.type.js";
+import PlatformPermissionsService, { PlatformPermissionName } from "../services/platformPermissions.service.js";
+
+/**
+ * Asserts that the current session must have the requested permissions.
+ *
+ * @example
+ * assertPlatformPermissions(req.session, "VIEW");
+ */
+export function assertPlatformPermissions(
+    session: ValidSessionType, 
+    requiredPermission: PlatformPermissionName | PlatformPermissionName[]
+): void {
+    if (!PlatformPermissionsService.has(
+        session.permissions.value, 
+        requiredPermission
+    )) {
+        throw new AdvancedError({
+            code: 401,
+            message: "Unauthorized"
+        })
+    }
+}
