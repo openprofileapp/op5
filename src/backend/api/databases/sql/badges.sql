3e50ad065676932405adb640e0d5ade0be088281
AvatarKage <contact@avatarkage.com>
2026-08-26 12:33:56 -0400

    removed comments and added index

--- src/backend/api/databases/sql/badges.sql
+++ src/backend/api/databases/sql/badges.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS badges (
-    id TEXT NOT NULL, -- User or asset id
+    id TEXT NOT NULL,
     type TEXT NOT NULL,
-    comment TEXT, -- The reason or milestone that earned the badge
+    comment TEXT,
     visibility TEXT NOT NULL DEFAULT 'public',
     date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
 
-    UNIQUE (id, type) -- Ensure users/assets do not have multiple of the same badges
+    UNIQUE (id, type)
 );
+
+CREATE INDEX IF NOT EXISTS idxBadgeTypes ON badges (type);
