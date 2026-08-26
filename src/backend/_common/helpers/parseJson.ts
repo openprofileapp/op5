1b9089cb6b621085efa74829fb6fdf3737ded1eb
AvatarKage <contact@avatarkage.com>
2026-08-26 12:29:20 -0400

    added string to json/array helper

---
+++ src/backend/_common/helpers/parseJson.ts
@@ -0,0 +1,11 @@
+export const parseJson = (value: unknown) => {
+    if (typeof value === "string") {
+        try {
+            return JSON.parse(value);
+        } catch {
+            return value;
+        }
+    }
+    
+    return value;
+};
