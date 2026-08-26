14f1089413c8dd4b8cbd659a5172637c5ead0cdb
AvatarKage <contact@avatarkage.com>
2026-08-26 12:38:23 -0400

    wip; broken service

--- src/backend/api/services/getUsersById.service.ts
+++ src/backend/api/services/getUsersById.service.ts
@@ -1,13 +1,10 @@
 import { AdvancedError } from "kage-library";
 
 import { db } from "../databases/db.js";
-import getBadgesById from "./getBadgesById.service.js";
 import { buildSqlInClause } from "../../../_common/helpers/sql.js";
 import { parseTags } from "../helpers/parseTags.js";
 import { GetUserType, UserType } from "../../../_common/types/user.type.js";
-import getInteractionsById from "./getInteractionsById.service.js";
 import { InteractionMethod, InteractionNameType } from "../../../_common/types/interaction.type.js";
-import getLinksById from "./getLinksById.service.js";
 import getUsernamesById from "./geUsernamesById.service.js";
 
 type Options = {
@@ -63,11 +60,9 @@ export default function getUsersById(
 
     const usernamesMap = getUsernamesById(userIds);
 
-    const badgesMap = getBadgesById(userIds);
 
-    const linksMap = getLinksById(userIds);
 
-    const interactionsMap = getInteractionsById(
+   /* const interactionsMap = getInteractionsById(
         userIds,
         { 
             method: options?.interactionMethod || "target",
@@ -75,7 +70,7 @@ export default function getUsersById(
             checkSourceInteraction: options?.getAs,
             countOnly: options?.interactionCountOnly
         }
-    );
+    );*/
 
     /*
     
@@ -94,16 +89,12 @@ export default function getUsersById(
         }
 
         return character;
-    });
-    */
+    });*/
 
     const formatUser = (row: UserType): GetUserType => ({
         ...row,
         tags: parseTags(row.tags),
         usernames: usernamesMap[row.id] || [],
-        badges: badgesMap[row.id] || [],
-        links: linksMap[row.id] || [],
-        interactions: interactionsMap[row.id] || {}
     });
 
     if (!isArray) {
