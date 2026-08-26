ca28e448a34b120a658d2d7381b00253eb1071db
AvatarKage <contact@avatarkage.com>
2026-08-26 12:38:32 -0400

    centered toast text

--- src/frontend/_common/components/ToastContainer.tsx
+++ src/frontend/_common/components/ToastContainer.tsx
@@ -55,7 +55,7 @@ export default function ToastContainer() {
                         `}
                     >
                         {t.icon && (<span className="flex items-center justify-center w-4 text-lg font-nerdfont leading-none shrink-0">{t.icon}</span>)}
-                        <div className="flex flex-col text-left">
+                        <div className="flex flex-col text-center">
                             <span className="font-medium text-sm leading-snug">{t.message}</span>
                             {t.subtext && (
                                 <span className="text-xs opacity-80 leading-snug mt-0.5">
