adb022dcfbb42aa60038512b2ba198bbd37a1024
AvatarKage <contact@avatarkage.com>
2026-08-27 10:06:27 -0400

    added sw route

--- src/backend/_common/routes/common.routes.ts
+++ src/backend/_common/routes/common.routes.ts
@@ -1,12 +1,13 @@
-import express, { Router } from 'express';
-import path from 'path';
+import express, { Router } from "express";
+import path from "path";
 
-import { config } from '../../../../app.config.js';
+import { config } from "../../../../app.config.js";
 
 const commonRoutes = Router();
 
 commonRoutes.use("/manifest.json", express.static(path.join(config.folders.public, "/manifest.json")));
 commonRoutes.use("/robots.txt", express.static(path.join(config.folders.public, "/robots.txt")));
 commonRoutes.use("/ads.txt", express.static(path.join(config.folders.public, "/ads.txt")));
+commonRoutes.use("/sw.js", express.static(path.join(config.folders.public, "/sw.js")));
 
 export default commonRoutes;
