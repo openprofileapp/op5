d2d76714c406bba8a0f39cee1b61d5900542b95f
AvatarKage <contact@avatarkage.com>
2026-08-27 13:00:57 -0400

    moved AlgorithmEventNameType to its own file

---
+++ src/_common/types/algorithm.type.ts
@@ -0,0 +1,19 @@
+export type AlgorithmEventNameType = 
+    "API" |
+    "VIEW" |
+    "READ" |
+    "CHAT" |
+    "LIKE" |
+    "UNLIKE" |
+    "FOLLOW" |
+    "UNFOLLOW" |
+    "COMMENT" |
+    "UNCOMMENT" |
+    "SHARE" |
+    "UPDATE" |
+    "REPORT" |
+    "ADD_TO_COLLECTION" |
+    "REMOVE_FROM_COLLECTION" |
+    "ADD_INTEREST" |
+    "REMOVE_INTEREST"
+;
