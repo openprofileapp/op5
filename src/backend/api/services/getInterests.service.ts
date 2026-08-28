6cf5060e553efc695461cd65aa4e5169c1e48905
AvatarKage <contact@avatarkage.com>
2026-08-28 06:16:16 -0400

    renamed getUserInterestsById to getInterestsService

--- src/backend/api/services/getInterests.service.ts
+++ src/backend/api/services/getInterests.service.ts
@@ -2,7 +2,7 @@ import { db } from "../databases/db.js";
 import { assertNotNull } from "../../../_common/asserts/notNull.assert.js";
 import { assertDbSuccess } from "../../../_common/asserts/dbSuccess.assert.js";
 
-export default function getUserInterestsById(id: string) {
+export default function getInterestsService(id: string) {
     assertNotNull(id);
 
     const result = db.users.query(
