dba9eca7cc5d827eb3d8f757989eab3debfd427d
AvatarKage <contact@avatarkage.com>
2026-08-28 09:31:37 -0400

    removed commented code block

--- src/backend/api/routes/interaction.routes.ts
+++ src/backend/api/routes/interaction.routes.ts
@@ -9,17 +9,4 @@ interactionRoutes.post("/", postInteractionController);
 
 interactionRoutes.get("/random/:type/:count", getRandomInteractionController);
 
-// interactionRoutes.get("/:userId", getInteractionsCount);
-// interactionRoutes.get("/:userId/:interaction", getInteractionsCount);
-
-// interactionRoutes.get("/:userId/following", getFollowing);
-// interactionRoutes.get("/:userId/followers", getFollowers);
-
-// interactionRoutes.get("/:sourceUserId/relationship/:targetUserId", getRelationship);
-// interactionRoutes.post("/:userId/follow/:targetUserId", postInteraction);
-
-// https://api.openprofile.app/v2/interactions
-// Once interactions are done, connect them to the context menu.
-// Ensure the interaction happens only if userId is req.session.userId
-
 export default interactionRoutes;
