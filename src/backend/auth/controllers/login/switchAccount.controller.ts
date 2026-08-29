f648f6a58dd710e0fb4c1fadafb59981012a7665
AvatarKage <contact@avatarkage.com>
2026-08-29 04:36:18 -0400

    renamed and polished switchAccountController

--- src/backend/auth/controllers/login/switchAccount.controller.ts
+++ src/backend/auth/controllers/login/switchAccount.controller.ts
@@ -7,86 +7,21 @@ import { db } from "../../databases/db.js";
 import { log } from "../../instances.js";
 import { SessionType } from "../../types/session.type.js";
 import { config } from "../../../../../app.config.js";
-import { id } from "../../../_common/instances.js";
-import { DateTime } from "luxon";
+import { assertDbSuccess } from "../../../../_common/asserts/dbSuccess.assert.js";
+import updateToken from "../../helpers/updateToken.js";
+import { i18n } from "../../../_common/instances.js";
 
-export const switchAccount = async (req: Request, res: Response) => {
-    const { userId } = req.params;
-    const delegationToken = req.cookies?.delegationToken;
-    const url = new URL(`https://${config.domains.main}`);
-    const refererUrl = req.get("Referer") || "/";
-
-    function updateSessionToken(userId: string, sessionId: string) {
-        const sessionToken = id.gen("TOKEN");
-        const in30Days = DateTime.now().toUTC().plus({ days: 30 }).toISO();
-
-        const hashedSessionToken = crypto.createHash("sha256").update(sessionToken).digest("hex");
-
-        const result = db.accounts.query(
-            `UPDATE sessions SET 
-                userId = ?,
-                sessionToken = ?, 
-                sessionTokenExpireDate = ?
-                WHERE sessionId = ? LIMIT 1`,
-            [
-                userId,
-                hashedSessionToken,
-                in30Days,
-                sessionId,
-            ]
-        );
-
-        if (!result.success) {
-            throw new AdvancedError({
-                code: 500,
-                message: "An error occurred while saving session token",
-                details: result.error
-            })
-        }
-
-        return {
-            type: "session",
-            value: sessionToken
-        };
-    }
-
-    function updateAccessToken(userId: string, sessionId: string) {
-        const accessToken = id.gen("TOKEN");
-        const in5Minutes = DateTime.now().toUTC().plus({ minutes: 5 }).toISO();
-
-        const hashedAccessToken = crypto.createHash("sha256").update(accessToken).digest("hex");
-
-        const result = db.accounts.query(
-            `UPDATE sessions SET 
-                userId = ?,
-                accessToken = ?, 
-                accessTokenExpireDate = ?
-                WHERE sessionId = ? LIMIT 1`,
-            [
-                userId,
-                hashedAccessToken,
-                in5Minutes,
-                sessionId,
-            ]
-        );
-
-        if (!result.success) {
-            throw new AdvancedError({
-                code: 500,
-                message: "An error occurred while saving session token",
-                details: result.error
-            })
-        }
-
-        return {
-            type: "session",
-            value: accessToken
-        };
-    }
-
+export const switchAccountController = async (req: Request, res: Response) => {
     try {
-        if (!delegationToken) {
-            return res.redirect(refererUrl);
+        const { redirect } = req.query;
+        const { userId } = req.params;
+
+        const delegationToken = req.cookies?.delegationToken;
+
+        const url = new URL(`https://${config.domains.main}`);
+
+        if (!delegationToken || !userId) {
+            return res.redirect(redirect as string || "/");
         }
 
         const hashedDelegationToken = crypto.createHash("sha256").update(delegationToken).digest("hex")
@@ -97,20 +32,14 @@ export const switchAccount = async (req: Request, res: Response) => {
             [hashedDelegationToken, userId]
         );
 
-        if (!result.success) {
-            throw new AdvancedError({
-                code: 500,
-                message: "An error occurred while fetching session",
-                details: result.error
-            })
-        }
+        assertDbSuccess(result);
 
-        const session = result.rows[0];
+        const row = result.rows[0];
 
-        const sessionTokenResult = updateSessionToken(userId as string, session.sessionId);
-        const accessTokenResult = updateAccessToken(userId as string, session.sessionId);
+        const accessToken = updateToken(row.sessionId, "ACCESS");
+        const sessionToken =  updateToken(row.sessionId, "SESSION");
 
-        res.cookie("sessionId", session.sessionId, {
+        res.cookie("sessionId", row.sessionId, {
             httpOnly: true,
             secure: true,
             sameSite: "none",
@@ -119,16 +48,7 @@ export const switchAccount = async (req: Request, res: Response) => {
             maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
         });
 
-        res.cookie("sessionToken", sessionTokenResult.value, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-            domain: `.${url.domain}`,
-            path: "/",
-            maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-        });
-
-        res.cookie("accessToken", accessTokenResult.value, {
+        res.cookie("accessToken", accessToken, {
             httpOnly: true,
             secure: true,
             sameSite: "none",
@@ -137,8 +57,17 @@ export const switchAccount = async (req: Request, res: Response) => {
             maxAge: 1000 * 60 * 5, // 5 minutes
         });
 
-        return res.redirect(refererUrl);
-    } catch (error) {
+        res.cookie("sessionToken", sessionToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            domain: `.${url.domain}`,
+            path: "/",
+            maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+        });
+
+        return res.redirect(redirect as string || "/");
+    } catch(error) {
         if (error instanceof AdvancedError) {
             log.db.error(error).save();
             return res.status(error.code).json({
@@ -147,6 +76,9 @@ export const switchAccount = async (req: Request, res: Response) => {
             });
         } else {
             log.unknown.error(error).save();
+            return res.status(500).json({
+                message: i18n.t("responses.unknown"),
+            });
         }
-    } 
+    }
 };
