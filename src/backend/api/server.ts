924485de591bb08b3c96ab0245e2ca006db94044
AvatarKage <contact@avatarkage.com>
2026-08-28 21:56:51 -0400

    polished api server.ts

--- src/backend/api/server.ts
+++ src/backend/api/server.ts
@@ -12,16 +12,16 @@ import { corsMiddleware } from "../_common/middlewares/cors.middleware.js";
 import { maintenanceMiddleware } from "../_common/middlewares/maintenance.middleware.js";
 import { fetchSessionMiddleware } from "./middlewares/fetchSession.middleware.js";
 import rateLimitMiddleware from "../_common/middlewares/rateLimit.middleware.js";
+import healthRoute from "../_common/routes/health.route.js";
 import userRoutes from "./routes/user.routes.js";
+import characterRoutes from "./routes/character.routes.js";
+import pinRoutes from "./routes/pin.routes.js";
 import inviteRoutes from "./routes/invite.routes.js";
 import interactionRoutes from "./routes/interaction.routes.js";
-import healthRoute from "../_common/routes/health.route.js";
-import characterRoutes from "./routes/character.routes.js";
 import webPushRoute from "./routes/webPush.route.js";
-import getUsersService from "./services/getUsers.service.js";
-import { parseJson } from "../_common/helpers/parseJson.js";
-import getInteractionsService from "./services/getInteractions.service.js";
 import statisticsRoute from "./routes/statistics.route.js";
+import auditRoute from "./routes/audit.route.js";
+
 
 /* 
 ————————————————————————————————————————————————————————————————
@@ -63,35 +63,54 @@ v3.use(
     userRoutes
 );
 
-/*v3.use(
+v3.use(
+    "/characters", 
+    fetchSessionMiddleware, 
+    rateLimitMiddleware(240), 
+    characterRoutes
+);
+
+v3.use(
     "/pins", 
     fetchSessionMiddleware, 
     rateLimitMiddleware(240), 
     pinRoutes
-);*/
+);
 
-// ADD A ACCESS TOKEN CHECK MIDDLEWARE middleware(access OR ApiSecret)
+// WARNING: Do NOT validate session here. It will cause a recursion with auth server
+v3.use(
+    "/invites", 
+    rateLimitMiddleware(240), 
+    inviteRoutes
+);
 
-v3.use("/characters", fetchSessionMiddleware, rateLimitMiddleware(240), characterRoutes);
+v3.use(
+    "/interactions", 
+    fetchSessionMiddleware, 
+    rateLimitMiddleware(30), 
+    interactionRoutes
+);
 
-// WARNING: Do NOT validate session here. It will cause a recursion with auth servers
-v3.use("/invites", rateLimitMiddleware(240), inviteRoutes); 
+v3.use(
+    "/webpush", 
+    fetchSessionMiddleware, 
+    rateLimitMiddleware(8), 
+    webPushRoute
+);
 
-v3.use("/interactions", fetchSessionMiddleware, rateLimitMiddleware(30), interactionRoutes);
-v3.use("/webpush", fetchSessionMiddleware, rateLimitMiddleware(8), webPushRoute);
-v3.use("/statistics", fetchSessionMiddleware, rateLimitMiddleware(240), statisticsRoute);
-// v3.use("/audits", ); // For fetching audits
-//v3.use("/audit", fetchSessionMiddleware, rateLimitMiddleware(240), auditRoute); // post.audits???
-//v3.use("/random", fetchSessionMiddleware, rateLimitMiddleware(240), randomRoutes);
-// DEVELOPER NEEDED: Add /themes
-// DEVELOPER NEEDED: Set up live-template saving. The 2MB is cause the limit could be a bit large? Experiment on this a bit
-// v3.use("/templates", express.json({ limit: "2mb" }), fetchSessionMiddleware, rateLimitMiddleware(240), randomRoutes);
-// 
+v3.use(
+    "/statistics", 
+    fetchSessionMiddleware, 
+    rateLimitMiddleware(240), 
+    statisticsRoute
+);
 
-// DEVELOPER NEEDED: When editing your profile, assign interests based on your uploaded content and user tags,
-// This should assist with getting a head start in what you'd like to see
-
-// MAYBE ADD INTERESTS ROUTE; /v3/interests/USER_ID?top=5 (ONLY LOGGED IN USER CAN GET THEIR INTERESTS)
+v3.use(
+    "/audit", 
+    fetchSessionMiddleware, 
+    rateLimitMiddleware(240), 
+    auditRoute
+);
 
 /* 
 ————————————————————————————————————————————————————————————————
@@ -120,27 +139,3 @@ cron.schedule("0 0 * * *", () => {
     log.cron.info("Running daily tasks...");
     log.cleanLogs();
 });
-
-/*
-const user = getUsersService({
-    id: "5019646586243236",
-    getAs: "5719552362357773"
-})
-
-log.unknown.info(user).save()
-*/
-/*const character = getPublishedCharactersService({
-    id: "6587823496323314",
-    getAs: "5719552362357773"
-})
-
-log.unknown.info(character).save()
-*//*
-const interactions = getInteractionsService({
-    includeItems: true,
-    ownerId: "5719552362357773",
-    getAs: "5719552362357773"
-})
-
-log.unknown.info(parseJson(interactions)).save()
-*/
