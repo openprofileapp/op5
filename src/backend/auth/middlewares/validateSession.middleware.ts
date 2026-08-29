fe25362c4a2f3c31ba3ce41f00d86435208c9768
AvatarKage <contact@avatarkage.com>
2026-08-29 04:39:00 -0400

    polished validateSessionMiddleware

--- src/backend/auth/middlewares/validateSession.middleware.ts
+++ src/backend/auth/middlewares/validateSession.middleware.ts
@@ -4,22 +4,24 @@ import { AdvancedError } from "kage-library";
 
 import { log } from "../instances.js";
 import validateSession from "../services/validateSession.service.js";
+import { i18n } from "../../_common/instances.js";
 
 export const validateSessionMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        // Validates and returns the session
         const response = await validateSession(req, res);
 
-        if (response.action === "REFRESH_PAGE") {
+        if ("action" in response && response.action === "REFRESH_PAGE") {
             return res.redirect(
                 req.originalUrl || "/"
             );
         }
 
-        req.session = response;
+        if (!("action" in response)) {
+            req.session = response;
+        }
 
         next();
-    } catch (error) {
+    } catch(error) {
         if (error instanceof AdvancedError) {
             log.db.error(error).save();
             return res.status(error.code).json({
@@ -28,6 +30,9 @@ export const validateSessionMiddleware = async (req: Request, res: Response, nex
             });
         } else {
             log.unknown.error(error).save();
+            return res.status(500).json({
+                message: i18n.t("responses.unknown"),
+            });
         }
-    } 
+    }
 };
