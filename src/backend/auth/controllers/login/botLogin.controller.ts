9c6cd3f97f42b4b1a92dcbd32aeeab86b41cf335
AvatarKage <contact@avatarkage.com>
2026-08-29 04:35:59 -0400

    renamed and polished botLoginController

--- src/backend/auth/controllers/login/botLogin.controller.ts
+++ src/backend/auth/controllers/login/botLogin.controller.ts
@@ -1,34 +1,38 @@
 import type { Request, Response } from "express";
-import getBotAccountByToken from "../../services/getBotAccountByToken.service.js";
 
 import { AdvancedError } from "kage-library";
 
+import { i18n } from "../../../_common/instances.js";
+import getBotAccountService from "../../services/getBotAccount.service.js";
 import { log } from "../../instances.js";
 
-export const botLogin = async (req: Request, res: Response) => {
+export const botLoginController = async (req: Request, res: Response) => {
     try {
         const authHeader = req.headers.authorization;
         
         if (!authHeader) {
-            return res.status(401).json({ error: "Missing Authorization header" });
+            throw new AdvancedError({
+                code: 401,
+                message: i18n.t("responses.authorizationHeader")
+            })
         }
 
         const authToken = authHeader.split(" ")[1];
+        const response = getBotAccountService(authToken);
 
-        // REJECT IF DOESN'T INCLUDE BEARER
-
-        const response = getBotAccountByToken(authToken);
-
-        return res.status(200).json({
-            ...response
-        });
-
-    } catch (error) {
+        return res.status(200).json(response);
+    } catch(error) {
         if (error instanceof AdvancedError) {
-            log.db.error(error.stack).save();
-            return res.status(error.code).json(error.message);
+            log.db.error(error).save();
+            return res.status(error.code).json({
+                id: error.id,
+                message: error.message
+            });
         } else {
-            console.log(error);
+            log.unknown.error(error).save();
+            return res.status(500).json({
+                message: i18n.t("responses.unknown"),
+            });
         }
     }
 };
