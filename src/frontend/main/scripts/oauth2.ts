c26ab14379d3fd3448df208f86301b180d22f847
AvatarKage <contact@avatarkage.com>
2026-08-30 09:22:17 -0400

    updated loginWithX

--- src/frontend/main/scripts/oauth2.ts
+++ src/frontend/main/scripts/oauth2.ts
@@ -67,17 +67,20 @@ export async function loginWithX(token: string) {
 
     const client_id = window.config.integrations.oauth2.x;
     const redirect_uri = `https://${window.config.domains.auth}/login/x`;
-    const scope = "users.read";
+    const scope = "users.read users.email tweet.read offline.access";
     const state = crypto.randomUUID();
     const verifier = crypto.randomUUID() + crypto.randomUUID();
 
     sessionStorage.setItem("x_oauth_state", state);
     sessionStorage.setItem("x_oauth_verifier", verifier);
+    
+    const rootDomain = window.location.hostname.split(".").slice(-2).join(".");
+    document.cookie = `x_oauth_verifier=${verifier}; path=/; domain=.${rootDomain}; max-age=600; secure; samesite=none`;
 
     const encoder = new TextEncoder();
     const data = encoder.encode(verifier);
     const digest = await crypto.subtle.digest("SHA-256", data);
-
+    
     const challenge = btoa(
         String.fromCharCode(...new Uint8Array(digest))
     )
@@ -96,6 +99,9 @@ export async function loginWithX(token: string) {
         "&code_challenge_method=S256";
 
     window.location.href = url;
+
+    sessionStorage.removeItem("x_oauth_state");
+    sessionStorage.removeItem("x_oauth_verifier");
 
     return { ok: true };
 }
