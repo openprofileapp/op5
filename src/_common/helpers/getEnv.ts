afd53acecfccef4a3acb3709eea9a281834cdb92
AvatarKage <contact@avatarkage.com>
2026-08-30 09:15:03 -0400

    added discord tokens env dev redirection

--- src/_common/helpers/getEnv.ts
+++ src/_common/helpers/getEnv.ts
@@ -23,6 +23,30 @@ export default function getEnv(key: string) {
             )
         }
     }
+
+    if (key === "INTEGRATION_DISCORD_PUBLIC_KEY") {
+        return config.isProduction 
+            ? process.env["INTEGRATION_DISCORD_PUBLIC_KEY"]
+            : process.env["INTEGRATION_DISCORD_DEV_PUBLIC_KEY"]
+    }
+
+    if (key === "INTEGRATION_DISCORD_CLIENT_ID") {
+        return config.isProduction 
+            ? process.env["INTEGRATION_DISCORD_CLIENT_ID"]
+            : process.env["INTEGRATION_DISCORD_DEV_CLIENT_ID"]
+    }
+
+    if (key === "INTEGRATION_DISCORD_CLIENT_SECRET") {
+        return config.isProduction 
+            ? process.env["INTEGRATION_DISCORD_CLIENT_SECRET"]
+            : process.env["INTEGRATION_DISCORD_DEV_CLIENT_SECRET"]
+    }
+
+    if (key === "INTEGRATION_DISCORD_BOT_TOKEN") {
+        return config.isProduction 
+            ? process.env["INTEGRATION_DISCORD_BOT_TOKEN"]
+            : process.env["INTEGRATION_DISCORD_DEV_BOT_TOKEN"]
+    }
     
     assertNotNull(key);
 
