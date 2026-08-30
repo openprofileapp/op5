b379528f7e6ce71e7a2ab1c8181029cf70422195
AvatarKage <contact@avatarkage.com>
2026-08-30 09:18:09 -0400

    fixed webpush not registering per account

--- src/backend/api/databases/sql/users/webpush.sql
+++ src/backend/api/databases/sql/users/webpush.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS webpush (
     userId TEXT NOT NULL,
-    endpoint TEXT UNIQUE,
-    keys TEXT UNIQUE,
+    endpoint TEXT NOT NULL,
+    keys TEXT NOT NULL,
     date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
 
-    UNIQUE (userId, endpoint, keys)
+    UNIQUE (userId, endpoint)
 );
