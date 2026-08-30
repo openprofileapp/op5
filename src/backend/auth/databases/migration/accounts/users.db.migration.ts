276415232ca09795f5e87fc3bca77b489eded52f
AvatarKage <contact@avatarkage.com>
2026-08-30 01:43:56 -0400

    fixed permissions migration

--- src/backend/auth/databases/migration/accounts/users.db.migration.ts
+++ src/backend/auth/databases/migration/accounts/users.db.migration.ts
@@ -10,6 +10,31 @@ db.accounts.transaction(q => {
     if (!result.success) return log.db.error(result.error).save();
 
     for (const d of result.rows) {
+        if (d.permissions === "295148468129306247231") {
+            d.permissions = PlatformPermissionsService.getRole("premium").value;
+        }
+
+        if (d.permissions === "1475740088846717550655") {
+            d.permissions = PlatformPermissionsService.getRole("partner").value;
+        }
+
+        if (d.permissions === "295159727130459242527") {
+            const permissions = [
+                ...PlatformPermissionsService.getRole("staff").array,
+                ...PlatformPermissionsService.getRole("moderator").array,
+            ];
+
+            d.permissions = PlatformPermissionsService.encode(permissions);
+        }
+
+        if (d.permissions === "36028797018963968") {
+            d.permissions = PlatformPermissionsService.getRole("admin").value
+        }
+
+        if (d.id === "8057185762390040" || d.id === "3912544802938547") {
+            d.permissions = PlatformPermissionsService.getRole("staff").value;
+        }
+
         const result = q(
             `INSERT INTO users (
                 id,
@@ -23,7 +48,7 @@ db.accounts.transaction(q => {
             [
                 d.id,
                 d.email,
-                PlatformPermissionsService.getRole("member").value,
+                d.permissions,
                 d.locale,
                 d.timezone,
                 d.suspended,
