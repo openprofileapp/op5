ea7b0f2bd9b178da1224ef04c7bd6f8ee27aa5f0
AvatarKage <contact@avatarkage.com>
2026-08-31 12:32:56 -0400

    updated websocket

--- src/frontend/main/scripts/websocket.ts
+++ src/frontend/main/scripts/websocket.ts
@@ -1,8 +1,13 @@
+import { banner } from "../../_common/scripts/banner.js";
 import { log } from "./main.js";
 
 export default class WsClient<TSend = unknown> {
     private ws: WebSocket;
     private queue: string[] = [];
+
+    private cooldownMap = new Map<string, number>();
+    private cooldownMs = 5000;
+    private seen503Bypass = false;
 
     constructor(url: string) {
         if (!url) {
@@ -19,17 +24,47 @@ export default class WsClient<TSend = unknown> {
         };
 
         this.ws.onmessage = (event: MessageEvent<string>) => {
+            const rawData = event.data;
+            const now = Date.now();
+            const lastSeen = this.cooldownMap.get(rawData);
+
+            if (lastSeen && now - lastSeen < this.cooldownMs) {
+                return;
+            }
+
+            this.cooldownMap.set(rawData, now);
+
+            setTimeout(() => {
+                this.cooldownMap.delete(rawData);
+            }, this.cooldownMs);
+            
             try {
                 const data = JSON.parse(event.data);
 
-                if (data.message === "connected") {
-                    // Call an action
+                if (
+                    data.action === "DISPLAY_503" && 
+                    window.location.pathname !== "/503"
+                ) {
+                    window.location.replace("/503");
+                }
+
+                if (
+                    data.action === "DISPLAY_503_BYPASS" &&
+                    !this.seen503Bypass
+                ) {
+                    this.seen503Bypass = true;
+
+                    banner.show(
+                        "Your permissions bypass the maximum connected user limit. OpenProfile may run slower than normal.",
+                        {
+                            type: "warning"
+                        }
+                    );
                 }
 
                 log.ws.info("Received from server:", data);
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             } catch (error) {
-                log.ws.error("Invalid JSON received:", event.data);
+                log.ws.error("Invalid JSON received:", event.data, error);
             }
         };
 
