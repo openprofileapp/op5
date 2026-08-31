f41c452c9a53dfc22f6e9f44a6d4f0e093282393
AvatarKage <contact@avatarkage.com>
2026-08-31 12:30:29 -0400

    revamped ComingSoon

--- src/frontend/_common/pages/ComingSoon.tsx
+++ src/frontend/_common/pages/ComingSoon.tsx
@@ -1,39 +1,71 @@
 import { useTranslation } from "react-i18next";
-
-import isGateway from "../helpers/isGateway.js";
+import { Link } from "react-router-dom";
 
 import Metadata from "../components/Metadata.js";
-import Navbar from "../../main/components/Navbar.js";
-import Footer from "../../main/components/Footer.js";
+import { isNightly, nightlyBaseUrl } from "../scripts/domains.js";
+import Marquee from "../components/Marquee.js";
 
 export default function ComingSoon() {
     const { t, ready: isTranslationReady } = useTranslation();
 
     if (!isTranslationReady) return null;
-    
+
     return (
         <>
             <Metadata
-                title="Coming Soon"
+                title={t("metadata.titles.construction")}
                 allowIndex="false"
             />
 
-            <div className="min-h-[calc(100vh-64px)] flex items-center justify-center">
-                <div className="rounded-full border border-base-300 flex flex-col items-center justify-center bg-base-100 w-100 h-100 text-center gap-4 text-2xl">
+            <div className="relative min-h-screen w-full flex items-center justify-center md:p-4 bg-base-200 overflow-hidden select-none"> 
+                
+                <Marquee 
+                    primaryText={`${t("pages.construction.marquee.1")} • ${t("pages.construction.marquee.2")} • ${t("pages.construction.marquee.3")} • `}
+                    secondaryText={`${t("pages.construction.marquee.2")} • ${t("pages.construction.marquee.1")} • ${t("pages.construction.marquee.3")} • `}
+                />
 
-                    <img className="rounded-full" width={192}
-                        src={`https://${isGateway() ? window.location.host : window.config.domains.cdn}${isGateway() ? "/cdn" : ""}/graphics/alice-happy.svg`}
-                    />
+                <div className="relative z-10 w-full h-screen md:h-auto md:w-128 md:max-w-full md:rounded-lg bg-base-100 p-6 flex flex-col items-center justify-center text-center gap-2 text-2xl border border-base-300">
+                    <div>
+                        <h3 className="font-nerdfont text-6xl text-center mb-4">
+                            
+                        </h3>
 
-                    <span>Coming Soon</span>
+                        <h3 className="text-center text-2xl font-bold">
+                            {t("metadata.titles.construction")}
+                        </h3>
 
-                    <a className="tooltip tooltip-bottom tooltip-accent"
-                        data-tip="Join to follow live-updates"
-                        href={window.config.metadata.urls.discord.joinUpdateRoles}
-                    >
-                        <span className="text-2xl font-nerdfont"></span>
-                    </a>
+                        <p className="text-center text-sm text-sub py-4">
+                            {!isNightly() && (t("pages.construction.subtext"))}
+                            {isNightly() && (t("pages.construction.subtextNightly"))}
+                        </p>
+                    </div>
 
+                    <div className="pt-2 flex flex-row gap-2 w-full">
+                        {!isNightly() && (
+                            <a
+                                className="btn btn-secondary flex-1"
+                                href={`${nightlyBaseUrl}${window.location.pathname}${window.location.search}`}
+                            >
+                                {t("pages.construction.nightly")}
+                            </a>
+                        )}
+
+                        <a
+                            className="btn btn-secondary flex-1"
+                            href={window.config.metadata.urls.discord.joinUpdateRoles}
+                        >
+                            {t("pages.construction.updates")}
+                        </a>
+                    </div>
+
+                    <div className="flex flex-row gap-2 w-full">
+                        <Link
+                            className="btn btn-accent flex-1"
+                            to="/"
+                        >
+                            {t("pages.goHome")}
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
