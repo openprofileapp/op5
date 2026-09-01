54e06e1542db13ec0c09d2f4e94bfa66f1d684a3
AvatarKage <contact@avatarkage.com>
2026-09-01 15:56:45 -0400

    updated permssions

--- src/_common/types/permissions.type.ts
+++ src/_common/types/permissions.type.ts
@@ -1,7 +1,16 @@
 export type PermissionsType = {
     userId: string;
     assetId: string;
+    title: string;
+    comment: string;
     permissions: string;
     addedBy: string;
     date: string;
 }
+
+export type GetPermissionType = Omit<
+    PermissionsType, 
+    "userId" | "assetId"
+> & {
+    isCollaborator: boolean;
+};
--- src/backend/api/databases/sql/users/permissions.sql
+++ src/backend/api/databases/sql/users/permissions.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS permissions (
     userId TEXT NOT NULL,
     assetId TEXT NOT NULL,
+    title TEXT,
+    comment TEXT,
     permissions TEXT DEFAULT '0',
     addedBy TEXT NOT NULL,
     date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
