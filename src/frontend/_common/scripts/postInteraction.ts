80c5ab3064c34e45f3f40f099b77623da9fc3b64
AvatarKage <contact@avatarkage.com>
2026-09-02 17:29:09 -0400

    moved postInteraction client

---
+++ src/frontend/_common/scripts/postInteraction.ts
@@ -0,0 +1,23 @@
+import { InteractionNameType } from "../../../_common/types/interaction.type.js";
+import { apiBaseUrl } from "./domains.js";
+
+export async function postInteraction(
+    targetId: string, 
+    type: InteractionNameType
+) {
+    try {
+        const response = await fetch(
+            `${apiBaseUrl}/v3/interactions`, 
+            { 
+                credentials: "include", 
+                method: "POST", 
+                headers: {"Content-Type": "application/json"}, 
+                body: JSON.stringify({ targetId, type })
+            }
+        );
+
+        return await response.json()
+    } catch (error) {
+        console.error('Failed to post interaction:', error);
+    }
+}
