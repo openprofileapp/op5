04d3ca67cb6f287edf7b43339ab4700861f8ed53
AvatarKage <contact@avatarkage.com>
2026-09-03 16:44:17 -0400

    polished MuteModal

--- src/frontend/main/components/modals/MuteModal.tsx
+++ src/frontend/main/components/modals/MuteModal.tsx
@@ -1,33 +1,78 @@
 import { useTranslation } from "react-i18next";
+import { useState, useRef, useImperativeHandle, forwardRef } from "react";
+
 import { toast } from "../../../_common/scripts/toast.js";
+import { GetUserItemType } from "../../../../_common/types/user.type.js";
 
-type Props = {
-    userId: string;
-    displayName: string;
-    isStaff: boolean
-};
+export interface MuteModalRef {
+    open: (data: GetUserItemType) => void;
+    close: () => void;
+}
 
-export default function MuteModal({ 
-    userId,
-    displayName,
-    isStaff
-}: Props) {
+const MuteModal = forwardRef<MuteModalRef>((_, ref) => {
     const { t, ready: isTranslationReady } = useTranslation();
 
-    if (!isTranslationReady) return null;
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+    const [data, setData] = useState<GetUserItemType>();
+
+    const [isContentSelected, setIsContentSelected] = useState<boolean>(false);
+    const [isInteractionSelected, setIsInteractionSelected] = useState<boolean>(false);
+    const [isMessageSelected, setIsMessageSelected] = useState<boolean>(false);
+
+    const resetState = () => {
+        setData(undefined);
+
+        setIsContentSelected(false);
+        setIsInteractionSelected(false);
+        setIsMessageSelected(false);
+    };
+
+    useImperativeHandle(ref, () => ({
+        open: (data) => {
+            setData(data);
+
+            setTimeout(() => {
+                dialogRef.current?.showModal();
+            }, 0);
+        },
+        close: () => {
+            dialogRef.current?.close();
+            
+            resetState();
+        }
+    }));
+
+    const handleClose = () => {
+        dialogRef.current?.close();
+
+        resetState();
+    };
+
+    if (!isTranslationReady || !data) return null;
 
     return (
-        <dialog id="mute" className="modal">
+        <dialog
+            ref={dialogRef}
+            className="modal"
+        >
             <div className="modal-box">
                 <form method="dialog">
-                    <button className="cursor-pointer absolute right-0 top-0 m-5 text-2xl font-nerdfont"></button>
+                    <button 
+                        type="button" 
+                        className="cursor-pointer absolute right-0 top-0 m-5 text-2xl font-nerdfont"
+                        onClick={handleClose}
+                    >
+                        
+                    </button>
                 </form>
+            
                 <h3 className="font-bold text-2xl text-center">
-                    {t("components.modals.mute.title")} {displayName}
+                    {t("components.modals.mute.title")} {data.displayName}
                 </h3>
 
                 <p className="pb-5 py-4 text-sub text-sm text-center">
-                    {t("components.modals.mute.subtext")} {displayName}:
+                    {t("components.modals.mute.subtext")} {data.displayName}:
                 </p>
                 
                 <div className="flex gap-5 pb-8 pt-4 flex-col">
@@ -36,9 +81,9 @@ export default function MuteModal({
                             <input
                                 type="checkbox"
                                 className="checkbox"
-                                // checked={false}
+                                checked={isContentSelected}
                                 onChange={(e) => {
-                                    // CALL HERE
+                                    setIsContentSelected(e.target.checked);
                                 }}
                             />
                         </label>
@@ -58,9 +103,9 @@ export default function MuteModal({
                             <input
                                 type="checkbox"
                                 className="checkbox"
-                                // checked={false}
+                                checked={isInteractionSelected}
                                 onChange={(e) => {
-                                    // CALL HERE
+                                    setIsInteractionSelected(e.target.checked);
                                 }}
                             />
                         </label>
@@ -80,9 +125,9 @@ export default function MuteModal({
                             <input
                                 type="checkbox"
                                 className="checkbox"
-                                // checked={false}
+                                checked={isMessageSelected}
                                 onChange={(e) => {
-                                    // CALL HERE
+                                    setIsMessageSelected(e.target.checked);
                                 }}
                             />
                         </label>
@@ -101,31 +146,38 @@ export default function MuteModal({
                 <div className="pt-2 flex gap-2 flex-row relative">
                     <button 
                         className="btn flex-1 bg-base-300 text-white border-[var(--color-base-300)]" 
-                        onClick={() => {
-                            (document.getElementById("mute") as HTMLDialogElement)?.close();
-                        }}
+                        onClick={handleClose}
                     >
                         {t("components.modals.close")}
                     </button>
 
-                    {/* Display loading class while awaiting API */}
                     <button
                         className={`btn flex-1 bg-accent text-white border-accent`}
                         onClick={() => {
-                            (document.getElementById("mute") as HTMLDialogElement)?.close();
+                            handleClose();
+
+                            // DEVELOPER NEEDED: Setup interaction (via hook) and loading
+
                             toast.show(
-                                `${t("components.modals.mute.result")} ${displayName}`, 
+                                `${t("components.modals.mute.result")} ${data.displayName}`, 
                                 { icon: "󰂛", type: "error" }
                             );
                         }}
                     >
-                        {t("components.modals.mute.title")} {displayName}
+                        {t("components.modals.mute.title")} {data.displayName}
                     </button>
                 </div>
             </div>
-            <form method="dialog" className="modal-backdrop">
-                <button>close</button>
+            <form 
+                method="dialog" 
+                className="modal-backdrop"
+                onClick={handleClose}
+            >
+                <button type="submit">close</button>
             </form>
         </dialog>
     );
-}
+});
+
+MuteModal.displayName = "MuteModal";
+export default MuteModal;
