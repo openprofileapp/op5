914ff6513335d5ca34f3bb63b9647bf333ae5758
AvatarKage <contact@avatarkage.com>
2026-09-03 16:44:41 -0400

    polished ReportModal

--- src/frontend/main/components/modals/ReportModal.tsx
+++ src/frontend/main/components/modals/ReportModal.tsx
@@ -1,170 +1,155 @@
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+import { useState, useRef, useImperativeHandle, forwardRef } from "react";
 
-import ProjectCard from "../ProjectCard.js";
-import ImageInput from "../ImageInput.js";
-import { useObjectURL } from "../../../_common/hooks/useObjectURL.hook.js";
+import { toast } from "../../../_common/scripts/toast.js";
+import { TypeableDropdownInput } from "../../../_common/components/TypeableDropdownInput.js";
+import { GetUserItemType } from "../../../../_common/types/user.type.js";
+import { GetPublishedCharacterItemType } from "../../../../_common/types/character.type.js";
 
-// Assign proper project owner
-// create sends to server (avatarUrl too)
+export interface ReportModalRef {
+    open: (data: GetUserItemType | GetPublishedCharacterItemType) => void;
+    close: () => void;
+}
 
-// On gif upload, remove it then prompt to join premium
-
-export default function ReportModal() {
+const ReportModal = forwardRef<ReportModalRef>((_, ref) => {
     const { t, ready: isTranslationReady } = useTranslation();
 
-    const [activeTab, setActiveTab] = useState("general");
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
 
-    const [avatar, setAvatar] = useState<File | null>(null);
-    const [banner, setBanner] = useState<File | null>(null);
+    const [data, setData] = useState<GetUserItemType | GetPublishedCharacterItemType>();
 
-    const avatarUrl = useObjectURL(avatar);
-    const bannerUrl = useObjectURL(banner);
+    const resetState = () => {
+        setData(undefined);
+    };
 
-    const [name, setName] = useState("");
-    const [slug, setSlug] = useState("");
-    const [about, setAbout] = useState("");
+    useImperativeHandle(ref, () => ({
+        open: (data) => {
+            setData(data);
 
-    function closeReportModal() {
-        (document.getElementById("report") as HTMLDialogElement)?.close();
-        setActiveTab("general");
-        setAvatar(null);
-        setBanner(null);
-        setName("");
-        setSlug("");
-        setAbout("");
-    }
-    
-    if (!isTranslationReady) return null;
+            setTimeout(() => {
+                dialogRef.current?.showModal();
+            }, 0);
+        },
+        close: () => {
+            dialogRef.current?.close();
+            
+            resetState();
+        }
+    }));
+
+    const handleClose = () => {
+        dialogRef.current?.close();
+
+        resetState();
+    };
+
+    if (!isTranslationReady || !data) return null;
 
     return (
-        <dialog id="report" className="modal">
-            <div className="modal-box max-w-235">
+        <dialog 
+            ref={dialogRef} 
+            className="modal overflow-visible"
+        >
+            <div className="modal-box overflow-visible">
                 <form method="dialog">
-                    <button className="cursor-pointer absolute right-0 top-0 m-5 text-2xl font-nerdfont"
-                        onClick={() => { closeReportModal() }}>
+                    <button 
+                        type="button" 
+                        className="cursor-pointer absolute right-0 top-0 m-5 text-2xl font-nerdfont"
+                        onClick={handleClose}
+                    >
+                        
                     </button>
                 </form>
-                <h3 className="font-bold text-2xl text-center">New Project</h3>
-                <p className="pt-2 pb-6 text-sm text-center opacity-60">Your project will be saved privately.</p>
+                
+                <h3 className="font-bold text-2xl text-center">
+                    {t("components.modals.report.title")} {data.displayName}
+                </h3>
 
-                <div className="flex flex-row gap-6">
-                    <div className="tabs tabs-lift flex w-full">
-                        <input type="radio" name="report-tabs" className="md:hidden tab bg-base-200 flex-1" aria-label="General" checked={activeTab === "general"} onChange={() => setActiveTab("general")}/>
-                            <div className="tab-content border-t-base-300 md:border-0 rounded-none overflow-x-hidden overflow-y-auto h-108">
-                                <fieldset className="fieldset w-full">
-                                <div className="flex gap-3">
-                                    <div className="w-32">
-                                        <label className="label mt-1 mb-1">Avatar</label>
-                                        <ImageInput
-                                            value={avatar}
-                                            onChange={setAvatar}
-                                            accept="image/png, image/jpeg, image/jpg, image/gif"
-                                            aspectRatio={1}
-                                            height="32"
-                                            width="32"
-                                            label="avatar"
-                                        />
-                                    </div>
-
-                                    <div className="w-full">
-                                        <label className="label mt-1 mb-1">Banner</label>
-                                        <ImageInput
-                                            value={banner}
-                                            onChange={setBanner}
-                                            accept="image/png, image/jpeg, image/jpg"
-                                            aspectRatio={2}
-                                            height="32"
-                                            width="full"
-                                            label="banner"
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="flex flex-col gap-1 mt-1">
-                                    <label className="label">Name</label>
-                                    <input 
-                                        id="report-name"
-                                        type="text"
-                                        className="input w-full" 
-                                        placeholder="What is your project's name?"
-                                        value={name}
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                </div>
-                                
-                                <div className="flex flex-col gap-1 mt-1">
-                                    <label className="label">Slug (ADD VALIDATION)</label>
-                                    <label className="input w-full validator">
-                                        openprofile.app/
-                                        <input
-                                            id="report-slug"
-                                            type="url"
-                                            placeholder="project-name"
-                                            value={slug}
-                                            onChange={(e) => setSlug(e.target.value)}
-                                        />
-                                    </label>
-                                </div>
-
-                                <div className="flex flex-col gap-1 mt-1">
-                                    <label className="label">About</label>                                
-                                    <textarea
-                                        id="report-about"
-                                        className="textarea h-21 w-full resize-none"
-                                        placeholder="What is your project about?"
-                                        value={about}
-                                        onChange={(e) => setAbout(e.target.value)}
-                                    />
-                                </div>
-                            </fieldset>
+                <p className="pb-5 py-4 text-sub text-sm text-center">
+                    ID: {data.id}
+                </p>
+                
+                <div className="flex gap-5 pb-4 pt-4 flex-col">
+                    <div className="flex gap-6 flex-row items-center">
+                        <div className="w-6 flex items-center justify-center text-xl font-nerdfont shrink-0">
+                            
                         </div>
 
-                        <input type="radio" name="report-tabs" className="md:hidden tab bg-base-200 flex-1" aria-label="Preview" checked={activeTab === "preview"} onChange={() => setActiveTab("preview")}/>
-                        <div className="md:hidden tab-content border-t-base-300 rounded-none overflow-x-hidden overflow-y-auto h-108">
-                            <br></br>
-                            <ProjectCard
-                                isPreview={true}
-                                banner={bannerUrl}
-                                name={name || "Untitled Project"}
-                                slug={slug || ""}
-                                owner={{
-                                    id: "5019646586243236",
-                                    username: "avatarkage",
-                                    name: "AvatarKage",
-                                    isVerified: false,
-                                }}
-                                about={about}
-                            />
+                        <div>
+                            {t("components.modals.report.rowTitle")}
                         </div>
-                    </div>
+                    </div>                    
+                </div>
 
-                    <div className="hidden md:block">
-                        <br></br>
-                        <ProjectCard
-                            isPreview={true}
-                            banner={bannerUrl}
-                            name={name || "Untitled Project"}
-                            slug={slug || ""}
-                            owner={{
-                                id: "5019646586243236",
-                                username: "avatarkage",
-                                name: "AvatarKage",
-                                isVerified: false,
-                            }}
-                            about={about}
+                <div className="flex flex-col gap-2">
+                    <legend className="flex flex-col items-start w-full fieldset-legend text-sm font-normal">
+                        {t("components.modals.report.type")}
+                        <TypeableDropdownInput
+                            options={[
+                                { id: "abuse", name: t("components.modals.report.abuse") },
+                                { id: "spam", name: t("components.modals.report.spam") },
+                                { id: "inappropriate", name: t("components.modals.report.inappropriate") },
+                                { id: "dmca", name: t("components.modals.report.dmca") },
+                                { id: "misuse", name: t("components.modals.report.misuse") },
+                                { id: "safety", name: t("components.modals.report.safety") },
+                                { id: "impersonation", name: t("components.modals.report.impersonation") },
+                                { id: "ai", name: t("components.modals.report.ai") },
+                                { id: "bug", name: t("components.modals.report.bug") },
+                                { id: "other", name: t("components.modals.report.other") }
+                            ]}
+                            placeholder={t("components.modals.report.placeholder")}
+                            typeable={false}
                         />
-                    </div>
+                    </legend>
+
+                    <legend className="flex flex-col items-start w-full fieldset-legend text-sm font-normal">
+                        {t("words.Description")}
+                        <textarea
+                            className="textarea resize-none bg-base-100 border border-base-300 w-full min-h-10 h-24 text-sm z-2"
+                            id={`report-description`}
+                            placeholder={t("components.modals.report.description")}
+                            spellCheck={false}
+                            autoCorrect="off"
+                            autoCapitalize="off"
+                        />
+                    </legend>
                 </div>
 
-                <div className="flex gap-3 flex-row w-full">
-                    <button className="btn btn-neutral mt-4 flex-1"
-                        onClick={() => { closeReportModal() }}>
-                        Close
+                <div className="pt-4 mb-5 flex gap-2 flex-row relative">
+                    <button
+                        className={`btn flex-1 bg-accent text-white border-accent`}
+                        onClick={() => {
+                            handleClose();
+
+                            // DEVELOPER NEEDED: Setup reports API and loading
+
+                            toast.show(
+                                `${t("components.modals.report.result")}`, 
+                                { icon: "", type: "success" }
+                            );
+                        }}
+                    >
+                        <div className="font-nerdfont leading-none absolute left-4 text-lg">
+                            
+                        </div>
+                        {t("components.modals.report.submit")}
                     </button>
-                    <button className="btn btn-accent mt-4 flex-4">Create</button>
                 </div>
+
+                <span className="flex justify-center text-sub text-xs">
+                    {t("components.modals.report.footer")}
+                </span>
             </div>
+            <form 
+                method="dialog" 
+                className="modal-backdrop"
+                onClick={handleClose}
+            >
+                <button type="submit">close</button>
+            </form>
         </dialog>
     );
-}
+});
+
+ReportModal.displayName = "ReportModal";
+export default ReportModal;
