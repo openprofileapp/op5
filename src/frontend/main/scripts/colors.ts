072dd26098338f24e8286254c2b83060e09a6cd4
AvatarKage <contact@avatarkage.com>
2026-09-08 19:16:11 -0400

    moved hexToRgba to dedicated script

---
+++ src/frontend/main/scripts/colors.ts
@@ -0,0 +1,9 @@
+export const hexToRgba = (hex: string, alpha = 1) => {
+    if (!hex) return "00000000";
+
+    const clean = hex.replace("#", "");
+    const r = parseInt(clean.substring(0, 2), 16);
+    const g = parseInt(clean.substring(2, 4), 16);
+    const b = parseInt(clean.substring(4, 6), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
