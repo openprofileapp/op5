ff264d3a2048b7cc7643e8f58ff3ad19d3a1ac3a
AvatarKage <contact@avatarkage.com>
2026-09-12 03:38:48 -0400

    enforced NOT NULL for position

--- src/backend/api/databases/sql/pins.sql
+++ src/backend/api/databases/sql/pins.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS pins (
     ownerId TEXT NOT NULL,
     assetId TEXT NOT NULL,
-    position INTEGER DEFAULT 0,
+    position INTEGER NOT NULL DEFAULT 0,
     date TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
 
     UNIQUE (ownerId, assetId)
