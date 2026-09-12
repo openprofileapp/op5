0911d7df74d044bcdf65c825aa4da70bca5030b8
AvatarKage <contact@avatarkage.com>
2026-09-12 03:37:32 -0400

    added MetadataType

---
+++ src/_common/types/metadata.type.ts
@@ -0,0 +1,11 @@
+export type MetadataType = {
+    url: string | null;
+    siteName: string | null;
+    icon: string | null;
+    title: string | null;
+    description: string | null;
+    image: string | null;
+    type: string | null;
+    accent: string | null;
+    jsonLd: Record<string, unknown>;
+}
