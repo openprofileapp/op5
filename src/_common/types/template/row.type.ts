6f8e0eb13914035044836eec407458e9820348df
AvatarKage <contact@avatarkage.com>
2026-09-16 01:32:58 -0400

    updated row type

--- src/_common/types/template/row.type.ts
+++ src/_common/types/template/row.type.ts
@@ -1,18 +1,9 @@
 import { GetFieldType } from "./field.type.js";
 
-export type RowNameType = 
-    | "text"
-    | "media"
-    | "split"
-    | "timeline"
-    | "calendar"
-;
-
 export type RowType = {
     assetId: string;
     rowId: string;
     blockId: string;
-    type: RowNameType;
     position: number;
     createdBy: string;
     createdDate: string;
