c5480fad94b4c5d4f5406b320ccaa6a85d7c9428
AvatarKage <contact@avatarkage.com>
2026-09-17 02:57:29 -0400

    added more preload rows

--- src/backend/api/databases/preload/rows.db.preload.ts
+++ src/backend/api/databases/preload/rows.db.preload.ts
@@ -7,9 +7,33 @@ const createdBy = "9534968913312158";
 
 const index: Partial<TemplateRowItemType>[] = [
     {
-        rowId: "SNOWFLAKE HERE",
+        rowId: "93861942229209089",
         blockId: "93861942229209088",
         position: 0,
+        createdBy,
+    },
+    {
+        rowId: "93861942229209090",
+        blockId: "93861942229209088",
+        position: 1,
+        createdBy,
+    },
+    {
+        rowId: "93861942229209091",
+        blockId: "93861942229209088",
+        position: 2,
+        createdBy,
+    },
+    {
+        rowId: "93861942229209092",
+        blockId: "93861942229209088",
+        position: 3,
+        createdBy,
+    },
+    {
+        rowId: "93861942229209093",
+        blockId: "93861942229209088",
+        position: 4,
         createdBy,
     }
 ];
