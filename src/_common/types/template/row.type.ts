897a33a4846fa1f6d59702bd2f634e36a2a399af
AvatarKage <contact@avatarkage.com>
2026-09-17 00:52:12 -0400

    updated row type

--- src/_common/types/template/row.type.ts
+++ src/_common/types/template/row.type.ts
@@ -1,7 +1,6 @@
 import { GetFieldType } from "./field.type.js";
 
-export type RowType = {
-    assetId: string;
+export type TemplateRowItemType = {
     rowId: string;
     blockId: string;
     position: number;
@@ -9,9 +8,29 @@ export type RowType = {
     createdDate: string;
 }
 
-export type GetRowType = Omit<
-    RowType, 
+export type RowItemType = {
+    assetId: string;
+    rowId: string;
+    blockId: string;
+    position: number;
+    createdBy: string;
+    createdDate: string;
+}
+export type GetTemplateRowItemType = Omit<
+    TemplateRowItemType, 
+    "blockId"
+> & {
+    fields: GetFieldType[];
+};
+
+export type GetRowItemType = Omit<
+    RowItemType, 
     "assetId" | "blockId"
 > & {
     fields: GetFieldType[];
 };
+
+export type GetRowType = {
+    items: RowItemType[],
+    count: number
+}
