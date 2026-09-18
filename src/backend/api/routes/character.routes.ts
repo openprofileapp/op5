3eb329fe20ae8f706e91f5115cf8b816e0109218
AvatarKage <contact@avatarkage.com>
2026-09-18 01:32:01 -0400

    updated character routes

--- src/backend/api/routes/character.routes.ts
+++ src/backend/api/routes/character.routes.ts
@@ -8,6 +8,18 @@ import { getTaggedPublishedCharacters } from "../controllers/characters/getTagge
 import { getRecentFollowingPublishedCharacters } from "../controllers/characters/getRecentFollowingPublishedCharacters.controller.js";
 import { getRecommendedPublishedCharacters } from "../controllers/characters/getRecommendedPublishedCharacters.controller.js";
 import { getRecommendedTaggedPublishedCharacters } from "../controllers/characters/getRecommendedTaggedPublishedCharacters.controller.js";
+import { insertBlock } from "../controllers/characters/updates/blocks/insertBlock.controller.js";
+import { positionBlocks } from "../controllers/characters/updates/blocks/positionBlocks.controller.js";
+import { deleteBlock } from "../controllers/characters/updates/blocks/deleteBlock.controller.js";
+import { insertCategories } from "../controllers/characters/updates/categories/insertCategory.controller.js";
+import { positionCategories } from "../controllers/characters/updates/categories/positionCategory.controller.js";
+import { deleteCategories } from "../controllers/characters/updates/categories/deleteCategory.controller.js";
+import { insertRows } from "../controllers/characters/updates/rows/insertRow.controller.js";
+import { positionRows } from "../controllers/characters/updates/rows/positionRow.controller.js";
+import { deleteRows } from "../controllers/characters/updates/rows/deleteRow.controller.js";
+import { insertFields } from "../controllers/characters/updates/fields/insertField.controller.js";
+import { positionFields } from "../controllers/characters/updates/fields/positionField.controller.js";
+import { deleteFields } from "../controllers/characters/updates/fields/deleteField.controller.js";
 
 const characterRoutes = Router();
 
@@ -19,5 +31,21 @@ characterRoutes.get("/tag/:tag", getTaggedPublishedCharacters);
 characterRoutes.get("/recent/following", getRecentFollowingPublishedCharacters);
 characterRoutes.get("/recommended", getRecommendedPublishedCharacters);
 characterRoutes.get("/recommended/:tag", getRecommendedTaggedPublishedCharacters);
+
+characterRoutes.post("/insert/:assetId/categories", insertCategories);
+characterRoutes.post("/update/:assetId/categories/positions", positionCategories);
+characterRoutes.post("/delete/:assetId/categories/:categoryId", deleteCategories);
+
+characterRoutes.post("/insert/:assetId/blocks", insertBlock);
+characterRoutes.post("/update/:assetId/blocks/positions", positionBlocks);
+characterRoutes.post("/delete/:assetId/blocks/:blockId", deleteBlock);
+
+characterRoutes.post("/insert/:assetId/rows", insertRows);
+characterRoutes.post("/update/:assetId/rows/positions", positionRows);
+characterRoutes.post("/delete/:assetId/rows/:rowId", deleteRows);
+
+characterRoutes.post("/insert/:assetId/fields", insertFields);
+characterRoutes.post("/update/:assetId/fields/positions", positionFields);
+characterRoutes.post("/delete/:assetId/fields/:fieldId", deleteFields);
 
 export default characterRoutes;
