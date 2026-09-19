3f29d4833581376bc6c9ad6ea61ea78fcdfc7929
AvatarKage <contact@avatarkage.com>
2026-09-19 18:57:46 -0400

    updated main

--- src/frontend/studio/main.tsx
+++ src/frontend/studio/main.tsx
@@ -5,6 +5,11 @@ import { HelmetProvider } from "react-helmet-async"
 import { I18nextProvider } from "react-i18next"
 
 import i18n from "../_common/i18n.js"
+import { ModalProvider } from "../_common/hooks/ModalContext.hook.js"
+
+import { apiBaseUrl, cdnBaseUrl } from "../_common/scripts/domains.js"
+import { banner } from "../_common/scripts/banner.js"
+import { verifySession } from "../_common/scripts/session.js"
 
 import "../_common/styles/tailwind.css";
 import "../_common/styles/app.css"
@@ -26,31 +31,106 @@ style.textContent = `
 
 document.head.appendChild(style);
 
-// Have a display 503 scripts that re-renders the full page when recieving
-// { action: "DISPLAY_503" }
-
 import Layout from "./Layout.js"
+
 import ToastContainer from "../_common/components/ToastContainer.js"
 import CaptchaPortal from "../_common/components/modals/CaptchaPortal.js"
 import Messages from "../_common/components/Messages.js"
 
 import Dashboard from "./pages/Dashboard.js"
 import Analytics from "./pages/Analytics.js"
+import Characters from "./pages/content/Characters.js"
+import Datasets from "./pages/Datasets.js"
+import Trash from "./pages/Trash.js"
+
 import CharacterTemplate from "./components/CharacterTemplate.js"
 
 import ComingSoon from "../_common/pages/ComingSoon.js"
 import NotFound from "../_common/pages/NotFound.js"
-import { authBaseUrl, cdnBaseUrl } from "../_common/scripts/domains.js"
+import setupWebPushNotifications from "../_common/scripts/webPush.js"
+
+// eslint-disable-next-line react-refresh/only-export-components
+function RootLayout() {
+    return (
+        <ModalProvider>
+            <Layout />
+        </ModalProvider>
+    );
+}
 
 async function bootstrap() {
-    const response = await fetch(
-        `${authBaseUrl}/session`,
-        {
-            credentials: "include",
-        }
-    );
+    await verifySession();
 
-    window.session = await response.json();
+    if (!localStorage.getItem("locale")) {
+        localStorage.setItem(
+            "locale", 
+            window.session.locale || window.config.metadata.locale
+        );
+    }
+
+    if (localStorage.getItem("locale")) {
+        await i18n.changeLanguage(localStorage.getItem("locale") as string);
+
+        if (
+            (
+                localStorage.getItem("locale")?.startsWith("zh") ||
+                localStorage.getItem("locale")?.startsWith("es") ||
+                localStorage.getItem("locale")?.startsWith("hi") ||
+                localStorage.getItem("locale")?.startsWith("ar") ||
+                localStorage.getItem("locale")?.startsWith("ru") ||
+                localStorage.getItem("locale")?.startsWith("id") ||
+                localStorage.getItem("locale")?.startsWith("ja")
+            ) &&
+            !localStorage.getItem("hasSeenLocaleBanner")
+        ) {
+            banner.show(
+                i18n.t("banners.locale"),
+                {
+                    type: "warning",
+                    closeAction: { 
+                        onClick: () => {
+                            localStorage.setItem("hasSeenLocaleBanner", "true");
+                        } 
+                    }
+                }
+            );
+        }
+    }
+
+    if (!localStorage.getItem("hasSeenBetaBanner")) {
+        banner.show(
+            i18n.t("banners.beta"),
+            {
+                type: "error",
+                button: { 
+                    label: "Join our Discord", 
+                    onClick: () => {
+                        window.open(window.config.metadata.urls.discord.main, "_blank");
+                    } 
+                },
+                closeAction: { 
+                    onClick: () => {
+                        localStorage.setItem("hasSeenBetaBanner", "true");
+                    } 
+                }
+            }
+        );
+    }
+
+    if (window.session.userId) {
+        const response = await fetch(
+            `${apiBaseUrl}/v3/users?id=${window.session.userId}`,
+            {
+                credentials: "include",
+            }
+        );
+
+        const data = await response.json()
+
+        window.session.user = data.items[0];
+
+        setupWebPushNotifications();
+    }
 
     ReactDOM.createRoot(document.getElementById("root")!).render(
         <React.StrictMode>
@@ -61,14 +141,19 @@ async function bootstrap() {
                         <CaptchaPortal siteKey={window.config.integrations.hcaptcha} />
                         <Messages />
                         <Routes>
-                            <Route path="/character" element={<CharacterTemplate />} />
+                            <Route path="/template" element={<CharacterTemplate />} />
 
-                            <Route element={<Layout />}>
+                            <Route element={<RootLayout />}>
                                 <Route path="/" element={<Navigate to="/dashboard" replace />} />
                                 <Route path="/dashboard" element={<Dashboard />} />
                                 <Route path="/analytics" element={<Analytics />} />
-                                <Route path="/content" element={<ComingSoon />} />
-                                {/* /template/84615986161318 (Template Editor) */}
+                                <Route path="/content/characters" element={<Characters />} />
+                                <Route path="/content/universes" element={<ComingSoon />} />
+
+                                <Route path="/templates" element={<ComingSoon />} />
+                                <Route path="/datasets" element={<Datasets />} />
+
+                                <Route path="/trash" element={<Trash />} />
 
                                 <Route path="/404" element={<NotFound />} />
                                 <Route path="*" element={<Navigate to="/404" replace />} />
