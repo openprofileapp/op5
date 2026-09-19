a5d7d970d75c472d002de0fdaafb2795739aca2e
AvatarKage <contact@avatarkage.com>
2026-09-19 18:51:26 -0400

    options -> dataset

--- src/backend/api/databases/sql/characters/fields/drafts.sql
+++ src/backend/api/databases/sql/characters/fields/drafts.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS draft_fields (
     type TEXT NOT NULL,
     label TEXT,
     placeholder TEXT,
-    options TEXT DEFAULT '[]',
+    dataset TEXT,
     guide TEXT,
     isLocked INTEGER NOT NULL DEFAULT 0,
     position INTEGER NOT NULL DEFAULT 0,
