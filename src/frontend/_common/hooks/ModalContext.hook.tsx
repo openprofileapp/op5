4e7dc5c73851bae50dfbd062fbf98c1c930f6d6e
AvatarKage <contact@avatarkage.com>
2026-09-19 18:56:51 -0400

    updated modal context hook

--- src/frontend/_common/hooks/ModalContext.hook.tsx
+++ src/frontend/_common/hooks/ModalContext.hook.tsx
@@ -5,42 +5,57 @@ import MuteModal, { MuteModalRef } from "../components/modals/MuteModal.js";
 import RestrictModal, { RestrictModalRef } from "../components/modals/RestrictModal.js";
 import BlockModal, { BlockModalRef } from "../components/modals/BlockModal.js";
 import ReportModal, { ReportModalRef } from "../components/modals/ReportModal.js";
+import TrashModal, { TrashModalRef } from "../components/modals/TrashModal.js";
 import ShareModal, { ShareModalRef } from "../components/modals/ShareModal.js";
 import CharacterModal, { CharacterModalRef } from "../components/modals/CharacterModal.js";
-import EditUserProfileModal, { EditUserProfileModalRef } from "../../main/components/modals/EditUserProfileModal.js";
+import EditUserProfileModal, { EditUserProfileModalRef } from "../components/modals/EditUserProfileModal.js";
 import { GetUserItemType } from "../../../_common/types/user.type.js";
+import DeleteModal, { DeleteModalRef } from "../components/modals/DeleteModal.js";
+import DatasetEditorModal, { DataEditorModalRef } from "../components/modals/DataEditorModal.js";
 
 interface ModalContextType {
     notificationsModal: {
-        open: (...args: Parameters<NotificationsModalRef["open"]>) => void;
+        open: (...args: Parameters<NotificationsModalRef["open"]>) => ReturnType<NotificationsModalRef["open"]>;
         close: () => void;
     };
     muteModal: {
-        open: (...args: Parameters<MuteModalRef["open"]>) => void;
+        open: (...args: Parameters<MuteModalRef["open"]>) => ReturnType<MuteModalRef["open"]>;
         close: () => void;
     };
     restrictModal: {
-        open: (...args: Parameters<RestrictModalRef["open"]>) => void;
+        open: (...args: Parameters<RestrictModalRef["open"]>) => ReturnType<RestrictModalRef["open"]>;
         close: () => void;
     };
     blockModal: {
-        open: (...args: Parameters<BlockModalRef["open"]>) => void;
+        open: (...args: Parameters<BlockModalRef["open"]>) => ReturnType<BlockModalRef["open"]>;
         close: () => void;
     };
     reportModal: {
-        open: (...args: Parameters<ReportModalRef["open"]>) => void;
+        open: (...args: Parameters<ReportModalRef["open"]>) => ReturnType<ReportModalRef["open"]>;
+        close: () => void;
+    };
+    trashModal: {
+        open: (...args: Parameters<TrashModalRef["open"]>) => ReturnType<TrashModalRef["open"]>;
+        close: () => void;
+    };
+    deleteModal: {
+        open: (...args: Parameters<DeleteModalRef["open"]>) => ReturnType<DeleteModalRef["open"]>;
         close: () => void;
     };
     shareModal: {
-        open: (...args: Parameters<ShareModalRef["open"]>) => void;
+        open: (...args: Parameters<ShareModalRef["open"]>) => ReturnType<ShareModalRef["open"]>;
         close: () => void;
     };
     characterModal: {
-        open: (...args: Parameters<CharacterModalRef["open"]>) => void;
+        open: (...args: Parameters<CharacterModalRef["open"]>) => ReturnType<CharacterModalRef["open"]>;
         close: () => void;
     };
     editUserProfileModal: {
         open: (data: GetUserItemType, onSave?: (updatedData: GetUserItemType) => void) => Promise<GetUserItemType | null | undefined>;
+        close: () => void;
+    };
+    dataEditorModal: {
+        open: (...args: Parameters<DataEditorModalRef["open"]>) => ReturnType<DataEditorModalRef["open"]>;
         close: () => void;
     };
 }
@@ -53,15 +68,18 @@ export function ModalProvider({ children }: { children: ReactNode }) {
     const restrictModalRef = useRef<RestrictModalRef>(null);
     const blockModalRef = useRef<BlockModalRef>(null);
     const reportModalRef = useRef<ReportModalRef>(null);
+    const trashModalRef = useRef<TrashModalRef>(null);
+    const deleteModalRef = useRef<DeleteModalRef>(null);
     const shareModalRef = useRef<ShareModalRef>(null);
     const characterModalRef = useRef<CharacterModalRef>(null);
     const editUserProfileModalRef = useRef<EditUserProfileModalRef>(null);
+    const dataEditorModalRef = useRef<DataEditorModalRef>(null);
 
     const value = useMemo(
         () => ({
             notificationsModal: {
                 open: (...args: Parameters<NotificationsModalRef["open"]>) => {
-                    notificationsModalRef.current?.open(...args);
+                    return notificationsModalRef.current?.open(...args);
                 },
                 close: () => {
                     notificationsModalRef.current?.close();
@@ -69,7 +87,7 @@ export function ModalProvider({ children }: { children: ReactNode }) {
             },
             muteModal: {
                 open: (...args: Parameters<MuteModalRef["open"]>) => {
-                    muteModalRef.current?.open(...args);
+                    return muteModalRef.current?.open(...args);
                 },
                 close: () => {
                     muteModalRef.current?.close();
@@ -77,7 +95,7 @@ export function ModalProvider({ children }: { children: ReactNode }) {
             },
             restrictModal: {
                 open: (...args: Parameters<RestrictModalRef["open"]>) => {
-                    restrictModalRef.current?.open(...args);
+                    return restrictModalRef.current?.open(...args);
                 },
                 close: () => {
                     restrictModalRef.current?.close();
@@ -85,7 +103,7 @@ export function ModalProvider({ children }: { children: ReactNode }) {
             },
             blockModal: {
                 open: (...args: Parameters<BlockModalRef["open"]>) => {
-                    blockModalRef.current?.open(...args);
+                    return blockModalRef.current?.open(...args);
                 },
                 close: () => {
                     blockModalRef.current?.close();
@@ -93,15 +111,31 @@ export function ModalProvider({ children }: { children: ReactNode }) {
             },
             reportModal: {
                 open: (...args: Parameters<ReportModalRef["open"]>) => {
-                    reportModalRef.current?.open(...args);
+                    return reportModalRef.current?.open(...args);
                 },
                 close: () => {
                     reportModalRef.current?.close();
                 },
             },
+            trashModal: {
+                open: (...args: Parameters<TrashModalRef["open"]>) => {
+                    return trashModalRef.current?.open(...args);
+                },
+                close: () => {
+                    trashModalRef.current?.close();
+                },
+            },
+            deleteModal: {
+                open: (...args: Parameters<DeleteModalRef["open"]>) => {
+                    return deleteModalRef.current?.open(...args);
+                },
+                close: () => {
+                    deleteModalRef.current?.close();
+                },
+            },
             shareModal: {
                 open: (...args: Parameters<ShareModalRef["open"]>) => {
-                    shareModalRef.current?.open(...args);
+                    return shareModalRef.current?.open(...args);
                 },
                 close: () => {
                     shareModalRef.current?.close();
@@ -109,7 +143,7 @@ export function ModalProvider({ children }: { children: ReactNode }) {
             },
             characterModal: {
                 open: (...args: Parameters<CharacterModalRef["open"]>) => {
-                    characterModalRef.current?.open(...args);
+                    return characterModalRef.current?.open(...args);
                 },
                 close: () => {
                     characterModalRef.current?.close();
@@ -122,21 +156,34 @@ export function ModalProvider({ children }: { children: ReactNode }) {
                 close: () => {
                     editUserProfileModalRef.current?.close();
                 },
+            },
+            dataEditorModal: {
+                open: (...args: Parameters<DataEditorModalRef["open"]>) => {
+                    return dataEditorModalRef.current?.open(...args);
+                },
+                close: () => {
+                    dataEditorModalRef.current?.close();
+                },
             }
         }),
         []
     );
 
     return (
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
         <ModalContext.Provider value={value}>
             <NotificationsModal ref={notificationsModalRef} />
             <MuteModal ref={muteModalRef} />
             <RestrictModal ref={restrictModalRef} />
             <BlockModal ref={blockModalRef} />
             <ReportModal ref={reportModalRef} />
+            <TrashModal ref={trashModalRef} />
+            <DeleteModal ref={deleteModalRef} />
             <ShareModal ref={shareModalRef} />
             <CharacterModal ref={characterModalRef} />
             <EditUserProfileModal ref={editUserProfileModalRef} />
+            <DatasetEditorModal ref={dataEditorModalRef} />
 
             {children}
         </ModalContext.Provider>
