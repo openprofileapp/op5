4a5c0f6615814a4228d6f582accfb90642d5eb02
AvatarKage <contact@avatarkage.com>
2026-09-19 18:47:43 -0400

    added deleteCharacter controller

---
+++ src/backend/api/controllers/characters/delete.controller.ts
@@ -0,0 +1,61 @@
+import type { Request, Response } from "express";
+import { AdvancedError } from "kage-library";
+
+import { assertBearer } from "../../../_common/asserts/bearer.assert.js";
+import { assertAccount } from "../../../_common/asserts/account.assert.js";
+import { assertPlatformPermissions } from "../../../_common/asserts/platformPermissions.assert.js";
+import { assertDbSuccess } from "../../../../_common/asserts/dbSuccess.assert.js";
+import { db } from "../../databases/db.js";
+import { DraftCharacterType } from "../../../../_common/types/character.type.js";
+import { i18n } from "../../../_common/instances.js";
+import { log } from "../../instances.js";
+
+export const deleteCharacter = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        await assertBearer(req);
+        assertAccount(req.session);
+        assertPlatformPermissions(req.session, "CREATE_ASSETS");
+
+        const getResult = db.characters.query<DraftCharacterType>(
+            "SELECT * FROM drafts WHERE id = ? LIMIT 1",
+            [id]
+        );
+
+        assertDbSuccess(getResult);
+
+        if (getResult.rowCount < 1) {
+            throw new AdvancedError({ 
+                code: 404, 
+                message: i18n.t("responses.characterNotFound")
+            });
+        }
+
+        if (getResult.rows[0].ownerId !== req.session.userId) {
+            throw new AdvancedError({
+                code: 401,
+                message: i18n.t("responses.unauthorized")
+            });
+        }
+
+        const postResult = db.characters.query<DraftCharacterType>(
+            "DELETE FROM drafts WHERE id = ? AND isDeleted = 1 LIMIT 1",
+            [id]
+        );
+
+        assertDbSuccess(postResult);
+
+        return res.status(200).json({ 
+            ok: true 
+        });
+    } catch (error) {
+        if (error instanceof AdvancedError) {
+            log.db.error(error).save();
+            return res.status(error.code).json({ id: error.id, message: error.message });
+        } else {
+            log.unknown.error(error).save();
+            return res.status(500).json({ message: i18n.t("responses.unknown") });
+        }
+    }
+};
